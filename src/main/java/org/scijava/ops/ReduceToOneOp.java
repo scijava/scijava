036fd87c25d174087d37f4c0a06f6b7c0a04e3ff
Christian Dietz <christian.dietz@knime.com>
2017-09-24 13:09:30 +0200

    WIP

--- src/main/java/org/scijava/ops/ReduceToOneOp.java
+++ src/main/java/org/scijava/ops/ReduceToOneOp.java
@@ -5,7 +5,5 @@ package org.scijava.ops;
  * reduce N->1 -- Function<Iterable<I>, O> -- Data apply(Iterable<Data> in) --
  * Note this is special case of {@link ReduceOp}.
  */
-public interface ReduceToOneOp<I, O> extends FunctionOp<Iterable<I>, O>,
-	ReduceOp<I, O>
-{
+public interface ReduceToOneOp<I, O> extends FunctionOp<Iterable<I>, O> {
 }

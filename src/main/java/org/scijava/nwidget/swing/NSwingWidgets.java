e80258e4078296c8313beffd47e805f0c8b51a1c
Curtis Rueden <ctrueden@wisc.edu>
2017-09-25 19:01:05 +0200

    NSwingWidgets: clean up a bit

--- src/main/java/org/scijava/nwidget/swing/NSwingWidgets.java
+++ src/main/java/org/scijava/nwidget/swing/NSwingWidgets.java
@@ -6,7 +6,7 @@ import javax.swing.JComponent;
 import org.scijava.nwidget.NWidget;
 import org.scijava.nwidget.NWidgets;
 
-/** Utility class for working with {@link NWidget}s. */
+/** Utility class for working with {@link NWidget}s and Swing. */
 public final class NSwingWidgets {
 
 	private NSwingWidgets() {
@@ -14,7 +14,7 @@ public final class NSwingWidgets {
 	}
 
 	/** Assigns the widget's description as the given component's tool tip. */
-	public static void setToolTip(final NSwingWidget widget, final JComponent c) {
+	public static void setToolTip(final NWidget widget, final JComponent c) {
 		final String desc = NWidgets.description(widget);
 		if (desc == null || desc.isEmpty()) return;
 		c.setToolTipText(desc);

55c972d27359da41dae3f44beac66dcbebb8f4e7
Curtis Rueden <ctrueden@wisc.edu>
2017-12-12 13:30:25 +0100

    PredicateOp: add missing marker interface

--- src/main/java/org/scijava/ops/PredicateOp.java
+++ src/main/java/org/scijava/ops/PredicateOp.java
@@ -3,7 +3,9 @@ package org.scijava.ops;
 
 import java.util.function.Predicate;
 
+import org.scijava.plugin.SciJavaPlugin;
+
 /** {@link Predicate}, as a plugin. */
 @FunctionalInterface
-public interface PredicateOp<IO> extends Predicate<IO> {
+public interface PredicateOp<IO> extends Predicate<IO>, SciJavaPlugin {
 }

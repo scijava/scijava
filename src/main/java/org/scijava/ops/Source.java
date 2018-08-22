f3139c12efba6aedfb9cea81359c012cbf1be6ba
David Kolb <david.kolb@knime.com>
2018-08-22 16:15:53 -0500

    Add some op adapters

---
+++ src/main/java/org/scijava/ops/Source.java
@@ -0,0 +1,12 @@
+package org.scijava.ops;
+
+import java.util.function.Supplier;
+
+public interface Source<O> extends Supplier<O> {
+	O create();
+	
+	@Override
+	default O get() {
+		return create();
+	}
+}

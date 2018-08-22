83642746c36b833125cfeca82092a878629bbaed
David Kolb <david.kolb@knime.com>
2018-08-22 14:48:28 -0500

    Add test for secondary inputs

--- src/test/java/org/scijava/ops/OpsTest.java
+++ src/test/java/org/scijava/ops/OpsTest.java
@@ -41,7 +41,9 @@ import org.scijava.Context;
 import org.scijava.ops.base.OpService;
 import org.scijava.ops.impl.math.Add.MathAddDoublesFunction;
 import org.scijava.ops.impl.math.Add.MathAddOp;
+import org.scijava.ops.impl.math.Power.MathPowerOp;
 import org.scijava.ops.impl.math.Sqrt.MathSqrtOp;
+import org.scijava.struct.StructInstance;
 import org.scijava.types.Nil;
 
 public class OpsTest {
@@ -61,7 +63,45 @@ public class OpsTest {
 		context = null;
 		ops = null;
 	}
-	
+
+	@Test
+	public void testSecondaryInputs() {
+		Class<Double> c = Double.class;
+		StructInstance<Function<Double, Double>> powerConstantFunctionStructInstance = ops.findStructInstance( //
+				new Nil<Function<Double, Double>>() {
+				}, //
+				new Type[] { MathPowerOp.class }, //
+				new Type[] { c, c }, //
+				new Type[] { c } //
+		);
+		powerConstantFunctionStructInstance.member("exponent").set(3.0);
+		Function<Double, Double> power3 = powerConstantFunctionStructInstance.object();
+		assert power3.apply(2.0).equals(8.0);
+		
+		BiFunction<Double, Double, Double> powerFunction = ops.findOp( //
+				new Nil<BiFunction<Double, Double, Double>>() {
+				}, //
+				new Type[] { MathPowerOp.class }, //
+				new Type[] { c, c }, //
+				new Type[] { c } //
+		);
+		assert powerFunction.apply(2.0, 3.0).equals(8.0);
+		
+		Class<double[]> cArray = double[].class;
+		StructInstance<Computer<double[], double[]>> powerConstantComputerStructInstance = ops.findStructInstance( //
+				new Nil<Computer<double[], double[]>>() {
+				}, //
+				new Type[] { MathPowerOp.class }, //
+				new Type[] { cArray, cArray, c }, //
+				new Type[] { cArray } //
+		);
+		powerConstantComputerStructInstance.member("exponent").set(3.0);
+		Computer<double[], double[]> power3Arrays = powerConstantComputerStructInstance.object();
+		double[] result = new double[3];
+		power3Arrays.compute(new double[] {1.0, 2.0, 3.0}, result);
+		assert arrayEquals(result, 1.0, 8.0, 27.0);
+	}
+
 	@Test
 	public void testUnaryOps() {
 		Class<Double> c = Double.class;
@@ -75,7 +115,7 @@ public class OpsTest {
 		// execute the function
 		double answer = sqrtFunction.apply(16.0);
 		assert 4.0 == answer;
-		
+
 		Class<double[]> cArray = double[].class;
 		Computer<double[], double[]> sqrtComputer = ops.findOp( //
 				new Nil<Computer<double[], double[]>>() {
@@ -86,8 +126,8 @@ public class OpsTest {
 		);
 		// execute the function
 		double[] result = new double[2];
-		sqrtComputer.compute(new double[] {16.0, 81.0}, result);
-		assert Arrays.deepEquals(Arrays.stream(result).boxed().toArray(Double[]::new), new Double[] { 4.0, 9.0 });
+		sqrtComputer.compute(new double[] { 16.0, 81.0 }, result);
+		assert arrayEquals(result, 4.0, 9.0);
 	}
 
 	@Test
@@ -104,7 +144,7 @@ public class OpsTest {
 		// execute the function
 		double answer = function.apply(1.0, 2.0);
 		assert 3.0 == answer;
-		
+
 		// look up a specific implementation
 		function = ops.findOp( //
 				new Nil<BiFunction<Double, Double, Double>>() {
@@ -130,6 +170,10 @@ public class OpsTest {
 		final double[] a2 = { 2, 4, 9 };
 		final double[] result = new double[a2.length];
 		computer.compute(a1, a2, result);
-		assert Arrays.deepEquals(Arrays.stream(result).boxed().toArray(Double[]::new), new Double[] { 5.0, 9.0, 16.0 });
+		assert arrayEquals(result, 5.0, 9.0, 16.0);
+	}
+	
+	private boolean arrayEquals(double[] arr1, Double... arr2) {
+		return Arrays.deepEquals(Arrays.stream(arr1).boxed().toArray(Double[]::new), arr2);
 	}
 }

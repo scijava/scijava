8d7f1009cf5a6bb57d67d75bb52e9f505ef56647
David Kolb <david.kolb@knime.com>
2018-08-27 11:32:54 -0500

    Add utility to retrieve functions

---
+++ src/main/java/org/scijava/ops/util/Functions.java
@@ -0,0 +1,40 @@
+package org.scijava.ops.util;
+
+import java.lang.reflect.Type;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+import org.scijava.ops.Op;
+import org.scijava.ops.base.OpService;
+import org.scijava.ops.types.Nil;
+
+/**
+ * Utility providing adaptation between {@link Op} types.
+ */
+public class Functions {
+
+	private Functions() {
+		// NB: Prevent instantiation of utility class.
+	}
+
+	public static <I, O> Function<I, O> unary(final OpService ops, final Class<? extends Op> opClass,
+			final Class<I> inputType, final Class<O> outputType) {
+		return ops.findOp( //
+				new Nil<Function<I, O>>() {
+				}, //
+				new Type[] { opClass }, //
+				new Type[] { inputType }, //
+				outputType);
+	}
+
+	public static <I1, I2, O> BiFunction<I1, I2, O> binary(final OpService ops, final Class<? extends Op> opClass,
+			final Class<I1> input1Type, final Class<I2> input2Type, final Class<O> outputType) {
+		return ops.findOp( //
+				new Nil<BiFunction<I1, I2, O>>() {
+				}, //
+				new Type[] { opClass }, //
+				new Type[] { input1Type, input2Type }, //
+				outputType);
+	}
+
+}

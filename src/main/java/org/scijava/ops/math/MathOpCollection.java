8f603c484abdb06d4452144afbbb75ad791f0890
David Kolb <david.kolb@knime.com>
2018-09-28 10:39:50 -0500

    Ignore .bat files

--- src/main/java/org/scijava/ops/math/MathOpCollection.java
+++ src/main/java/org/scijava/ops/math/MathOpCollection.java
@@ -24,6 +24,7 @@ public class MathOpCollection {
 	@Parameter(key = "result", type = ItemIO.OUTPUT)
 	public static final BiFunction<Double, Double, Double> addDoublesFunction = (x, y) -> x + y;
 
+	// Set this priority to extremely high for now to test that the op with the highest priority is actually retrieved
 	@OpField(names = MathOps.ADD, priority = Priority.EXTREMELY_HIGH)
 	@Parameter(key = "number1")
 	@Parameter(key = "number2")

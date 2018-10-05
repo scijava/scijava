5474d713263689e5bf652b1d2fda35887850ca2c
David Kolb <david.kolb@knime.com>
2018-10-05 15:10:59 -0500

    Add utility to do certain type modifications

---
+++ src/main/java/org/scijava/ops/transform/TypeModUtils.java
@@ -0,0 +1,239 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2014 - 2018 ImageJ developers.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package org.scijava.ops.transform;
+
+import java.lang.reflect.Array;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+import org.scijava.util.Types;
+
+/**
+ * Utility class to do modification on {@link Type}s.
+ * 
+ * @author David Kolb
+ */
+public final class TypeModUtils {
+
+	private TypeModUtils() {
+		// NB: prevent instantiation of utility class.
+	}
+
+	// -- Utility methods --
+	
+
+	/**
+	 * Attempts to perform "unlifting" of the specified types. See {@link #unliftParameterizedType(Type, Class, Class, Integer...)}.
+	 * The passed arry of types will be mutated inplace. Returns a boolean indicating if the list changed. 
+	 * 
+	 * @param types
+	 * @param searchRawType
+	 * @param unliftRawArgType
+	 * @param argIndices
+	 * @return
+	 */
+	public static boolean unliftParameterizedTypes(Type[] types, Class<?> searchRawType, Class<?> unliftRawArgType, Integer... argIndices) {
+		return mutateTypes(types, t -> unliftParameterizedType(t, searchRawType, unliftRawArgType, argIndices));
+	}
+	
+	/**
+	 * Attempts to "unlift" the type arguments of the specified parameterized type if:
+	 * <ul>
+	 * <li>The type is a {@link ParameterizedType}</li>
+	 * <li>The types raw type equals the search raw type</li>
+	 * <li>The raw type of the type argument equals the specified unlift raw argument type</li>
+	 * </ul>
+	 * Otherwise, null will be returned indicating that "unlifting" is not possible.
+	 * The list of integers specifies which type arguments should be considered. If non are given,
+	 * all arguments will be processed. If arrays (reified or generic) should be "unlifted", 
+	 * {@link Array}.class can be passed as the unlift raw arg type. Also see
+	 * {@link #unliftType(Type, Class)}.</br></br>
+	 * E.g.
+	 * <pre>
+	 * The type:
+	 *    Function&lt;Iterable&lt;Double&gt;, String[]&gt;
+	 *    
+	 * With: searchRawType = Function.class and unliftRawArgType = Iterable.class
+	 * will result in:
+	 *    Function&lt;Double, String[]&gt;
+	 *    
+	 * With: searchRawType = Function.class and unliftRawArgType = Array.class
+	 * will result in:
+	 *    Function&lt;Iterable&lt;Double&gt;, String&gt;
+	 *    
+	 * </pre>
+	 * 
+	 * 
+	 * @param type the type to "unlift"
+	 * @param searchRawType the raw type to check for
+	 * @param unliftRawArgType the raw type of type arguments that should be unlifted
+	 * @param argIndices indices of type arguments to process
+	 * @return "unlifted" type or null if not possible
+	 */
+	public static Type unliftParameterizedType(Type type, Class<?> searchRawType, Class<?> unliftRawArgType, Integer... argIndices) {
+		if (type instanceof ParameterizedType) {
+			ParameterizedType casted = (ParameterizedType) type;
+			
+			if (Types.raw(type).equals(searchRawType)) {
+				Type[] typeArgs = casted.getActualTypeArguments();
+				boolean hit = unliftTypes(typeArgs, unliftRawArgType, argIndices);
+				
+				if(hit) {
+					return Types.parameterize(searchRawType, typeArgs);
+				}
+			}
+		} 
+		return null;
+	}
+	
+	/**
+	 * Attempts to perform "unlifting" of the specified types. See {@link #unliftType(Type, Class)}.
+	 * The passed arry of types will be mutated inplace. Returns a boolean indicating if the list changed. 
+	 * 
+	 * @param types
+	 * @param unliftRawType
+	 * @param argIndices
+	 * @return
+	 */
+	public static boolean unliftTypes(Type[] types, Class<?> unliftRawType, Integer... argIndices) {
+		return mutateTypes(types, t -> unliftType(t, unliftRawType), argIndices);
+	}
+	
+	/**
+	 * Attempts to "unlift" the specified type directly if its raw type matches the specified unlift raw type.
+	 * "Unlifiting" will be performed in two cases:
+	 * <ol>
+	 * <li>The specified type is an array type (either reified or generic, pass {@link Array}.class as unlift raw type). 
+	 * Then the component type will be returned.</li>
+	 * <li>The specified type is a parameterized type. Then the first type argument will be returned if the parameterized
+	 * types raw type equals the unlift raw type and it has exactly one type argument.</li>
+	 * </ol>
+	 * Otherwise null will be returned indicating that "unlifting" is not possible.</br></br>
+	 * E.g.
+	 * <pre>
+	 * The type:
+	 *    Iterable&lt;Double&gt;
+	 *    
+	 * With: unliftRawType = Iterable.class
+	 * will result in:
+	 *    Double
+	 *    
+	 * The type:
+	 *    Double[]
+	 *    
+	 * With: unliftRawType = Array.class
+	 * will result in:
+	 *    Double
+	 * </pre>
+	 * 
+	 * @param type the type to "unlift"
+	 * @param unliftRawType the raw type to check for
+	 * @return "unlifted" type or null if not possible
+	 */
+	public static Type unliftType(Type type, Class<?> unliftRawType) {
+		if (unliftRawType.equals(Array.class)) {
+			return Types.component(type);
+		} else if (type instanceof ParameterizedType) {
+			ParameterizedType casted = (ParameterizedType) type;
+			
+			if (Types.raw(casted).equals(unliftRawType)) {
+				Type[] typeArgs = casted.getActualTypeArguments();
+				if (typeArgs.length == 1) {
+					return typeArgs[0];
+				}
+			}
+		}
+		return null;
+	}
+
+
+	/**
+	 * Attempts to perform raw type replacement of the specified types. See {@link #replaceRawType(Type, Class, Class)}.
+	 * The passed array of types will be mutated inplace. Returns a boolean indicating if the list changed. 
+	 * 
+	 * @param types
+	 * @param searchClass
+	 * @param replacement
+	 * @param indices
+	 * @return
+	 */
+	public static boolean replaceRawTypes(Type[] types, Class<?> searchClass, Class<?> replacement, Integer... indices) {
+		return mutateTypes(types, t -> replaceRawType(t, searchClass, replacement), indices);
+	}
+
+	/**
+	 * Attempts to exchange the raw type of the specified type with the specified replacement if the original 
+	 * raw type equals the search class. This method does not check if the returned type is actually permitted
+	 * (i.e. if the passed type is a parameterized type, the replacement may have a different amount of type
+	 * arguments). If type is a class, the replacement will be returned if the class equals the search class.
+	 * If type is a parameterized type, its raw type will be exchanged.
+	 * 
+	 * @param type the type whose raw type should be replaced
+	 * @param searchClass the raw type to check for
+	 * @param replacement the replacement type
+	 * @return
+	 */
+	public static Type replaceRawType(Type type, Class<?> searchClass, Class<?> replacement) {
+		if (type instanceof ParameterizedType) {
+			ParameterizedType casted = (ParameterizedType) type;
+			Class<?> rawType = Types.raw(type);
+			
+			if (rawType.equals(searchClass)) {
+				return Types.parameterize(replacement, casted.getActualTypeArguments());
+			}
+		} else if (type instanceof Class) {
+			if (((Class<?>) type).equals(searchClass)) {
+				return replacement;
+			}
+		}
+		return null;
+	}
+	
+	private static boolean mutateTypes(Type[] types, Function<Type, Type> typeFunc, Integer... indices) {
+		List<Integer> is = Arrays.asList(indices);
+		
+		boolean hit = false;
+		for (int i = 0; i < types.length; i++) {
+			if (!is.contains(i) && !is.isEmpty()) {
+				continue;
+			}
+			Type replaced = typeFunc.apply(types[i]);
+			
+			if (replaced != null) {
+				types[i] = replaced;
+				hit = true;
+			}
+		}
+		return hit;
+	}
+}
---
+++ src/test/java/org/scijava/ops/util/TypeModUtilsTest.java
@@ -0,0 +1,156 @@
+/*
+ * #%L
+ * SciJava Operations: a framework for reusable algorithms.
+ * %%
+ * Copyright (C) 2018 SciJava developers.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package org.scijava.ops.util;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.lang.reflect.Array;
+import java.lang.reflect.Type;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+import org.junit.Test;
+import org.scijava.ops.core.Computer;
+import org.scijava.ops.transform.TypeModUtils;
+import org.scijava.ops.types.Nil;
+
+public class TypeModUtilsTest {
+
+	@Test
+	public <A, B, C> void replaceRaw() {
+		Nil<Iterable<A>> n1 = new Nil<Iterable<A>>() {
+		};
+		Nil<Function<Double, String>> n2 = new Nil<Function<Double, String>>() {
+		};
+		Nil<A> n3 = new Nil<A>() {
+		};
+		Nil<B[]> n4 = new Nil<B[]>() {
+		};
+		Nil<Supplier<? extends Number>> n5 = new Nil<Supplier<? extends Number>>() {
+		};
+		Nil<Consumer<C>> n6 = new Nil<Consumer<C>>() {
+		};
+
+		Type[] types = new Type[] { Double.class, String.class, Iterable.class, String.class };
+		assertTrue(TypeModUtils.replaceRawTypes(types, String.class, Double.class));
+		assertTrue(Arrays.deepEquals(types, new Type[] { Double.class, Double.class, Iterable.class, Double.class }));
+		
+		types = new Type[] { Double.class, String.class, Iterable.class, String.class };
+		assertTrue(TypeModUtils.replaceRawTypes(types, String.class, Double.class, 1));
+		assertTrue(Arrays.deepEquals(types, new Type[] { Double.class, Double.class, Iterable.class, String.class }));
+		
+		types = new Type[] { Double.class, String.class, Iterable.class, String.class };
+		assertTrue(TypeModUtils.replaceRawTypes(types, String.class, Double.class, 1, 3));
+		assertTrue(Arrays.deepEquals(types, new Type[] { Double.class, Double.class, Iterable.class, Double.class }));
+
+		types = new Type[] { n1.getType(), n2.getType(), n3.getType(), n4.getType(), n5.getType(), n6.getType() };
+		assertTrue(TypeModUtils.replaceRawTypes(types, Function.class, Function.class));
+		assertTrue(Arrays.deepEquals(types,
+				new Type[] { n1.getType(), n2.getType(), n3.getType(), n4.getType(), n5.getType(), n6.getType() }));
+
+		types = new Type[] { n1.getType(), n2.getType(), n3.getType(), n4.getType(), n5.getType(), n6.getType() };
+		assertTrue(TypeModUtils.replaceRawTypes(types, Function.class, Computer.class));
+		Nil<Computer<Double, String>> y2 = new Nil<Computer<Double, String>>() {
+		};
+		assertTrue(Arrays.deepEquals(types,
+				new Type[] { n1.getType(), y2.getType(), n3.getType(), n4.getType(), n5.getType(), n6.getType() }));
+
+		types = new Type[] { n1.getType(), n2.getType(), n3.getType(), n4.getType(), n5.getType(), n6.getType() };
+		assertTrue(TypeModUtils.replaceRawTypes(types, Supplier.class, Iterable.class));
+		Nil<Iterable<? extends Number>> y5 = new Nil<Iterable<? extends Number>>() {
+		};
+		assertTrue(Arrays.deepEquals(types,
+				new Type[] { n1.getType(), n2.getType(), n3.getType(), n4.getType(), y5.getType(), n6.getType() }));
+
+		types = new Type[] { n1.getType(), n2.getType(), n3.getType(), n4.getType(), n5.getType(), n6.getType() };
+		assertTrue(TypeModUtils.replaceRawTypes(types, Consumer.class, List.class));
+		Nil<List<C>> y6 = new Nil<List<C>>() {
+		};
+		assertTrue(Arrays.deepEquals(types,
+				new Type[] { n1.getType(), n2.getType(), n3.getType(), n4.getType(), n5.getType(), y6.getType() }));
+
+		types = new Type[] { n1.getType(), n2.getType(), n3.getType(), n4.getType(), n5.getType(), n6.getType() };
+		assertFalse(TypeModUtils.replaceRawTypes(types, Integer.class, List.class));
+		assertTrue(Arrays.deepEquals(types,
+				new Type[] { n1.getType(), n2.getType(), n3.getType(), n4.getType(), n5.getType(), n6.getType() }));
+	}
+
+	@Test
+	public <A, B, C> void unliftParameterized() {
+		Nil<Computer<A, B[]>> n1 = new Nil<Computer<A, B[]>>() {
+		};
+		Nil<Function<Double, String[]>> n2 = new Nil<Function<Double, String[]>>() {
+		};
+		Nil<Function<C[], Iterable<String>>> n3 = new Nil<Function<C[], Iterable<String>>>() {
+		};
+		Nil<Computer<C[], Function<String, Integer>>> n4 = new Nil<Computer<C[], Function<String, Integer>>>() {
+		};
+		
+		Nil<Function<Double, String>> y2 = new Nil<Function<Double, String>>() {
+		};
+		Nil<Function<C, Iterable<String>>> y3 = new Nil<Function<C, Iterable<String>>>() {
+		};
+		Nil<Function<C[], String>> y31 = new Nil<Function<C[], String>>() {
+		};
+
+		Type[] types = new Type[] { n1.getType(), n2.getType(), n3.getType() };
+		assertFalse(TypeModUtils.unliftParameterizedTypes(types, Computer.class, Iterable.class));
+		assertTrue(Arrays.deepEquals(types, new Type[] { n1.getType(), n2.getType(), n3.getType() }));
+		
+		types = new Type[] { n1.getType(), n2.getType(), n4.getType() };
+		assertFalse(TypeModUtils.unliftParameterizedTypes(types, Computer.class, Function.class));
+		assertTrue(Arrays.deepEquals(types, new Type[] { n1.getType(), n2.getType(), n4.getType() }));
+		
+		types = new Type[] { n1.getType(), n2.getType(), n3.getType() };
+		assertTrue(TypeModUtils.unliftParameterizedTypes(types, Function.class, Iterable.class));
+		assertTrue(Arrays.deepEquals(types, new Type[] { n1.getType(), n2.getType(), y31.getType() }));
+		
+		types = new Type[] { n1.getType(), n2.getType(), n3.getType() };
+		assertTrue(TypeModUtils.unliftParameterizedTypes(types, Function.class, Array.class));
+		assertTrue(Arrays.deepEquals(types, new Type[] { n1.getType(), y2.getType(), y3.getType() }));
+		
+		types = new Type[] { n1.getType(), n2.getType(), n3.getType() };
+		assertTrue(TypeModUtils.unliftParameterizedTypes(types, Function.class, Array.class, 1));
+		assertTrue(Arrays.deepEquals(types, new Type[] { n1.getType(), y2.getType(), n3.getType() }));
+		
+		types = new Type[] { n1.getType(), n2.getType(), n3.getType() };
+		assertTrue(TypeModUtils.unliftParameterizedTypes(types, Function.class, Array.class, 0));
+		assertTrue(Arrays.deepEquals(types, new Type[] { n1.getType(), n2.getType(), y3.getType() }));
+		
+		types = new Type[] { n1.getType(), n2.getType(), n3.getType() };
+		assertTrue(TypeModUtils.unliftParameterizedTypes(types, Function.class, Array.class, 0, 1));
+		assertTrue(Arrays.deepEquals(types, new Type[] { n1.getType(), y2.getType(), y3.getType() }));
+
+	}
+}

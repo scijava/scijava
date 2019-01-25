99feb7e33a1a5b30d7207bd49e136b85d7c6987d
David Kolb <david.kolb@knime.com>
2019-01-25 17:31:11 +0100

    Remove parameter annotations as they are not needed anymore

--- src/main/java/org/scijava/ops/math/MathOpCollection.java
+++ src/main/java/org/scijava/ops/math/MathOpCollection.java
@@ -10,43 +10,23 @@ import org.scijava.param.Parameter;
 import org.scijava.plugin.Plugin;
 import org.scijava.struct.ItemIO;
 
-// TODO: Derive parameters from the one abstract method of the functional interface in question.
-// This also will tell us the number of inputs vs. outputs. But!
-// It will NOT tell us if an argument is "BOTH".
-// And it will NOT tell us the preferred NAME of the argument.
-
 @Plugin(type = OpCollection.class)
 public class MathOpCollection {
 
 	@OpField(names = MathOps.ADD, priority = Priority.LOW)
-	@Parameter(key = "number1")
-	@Parameter(key = "number2")
-	@Parameter(key = "result", type = ItemIO.OUTPUT)
 	public static final BiFunction<Double, Double, Double> addDoublesFunction = (x, y) -> x + y;
 
 	// Set this priority to extremely high for now to test that the op with the highest priority is actually retrieved
 	@OpField(names = MathOps.ADD, priority = Priority.EXTREMELY_HIGH)
-	@Parameter(key = "number1")
-	@Parameter(key = "number2")
-	@Parameter(key = "result", type = ItemIO.OUTPUT)
 	public static final BinaryOperator<Double> addDoublesOperator = (x, y) -> x + y;
 
 	@OpField(names = MathOps.SUB)
-	@Parameter(key = "number1")
-	@Parameter(key = "number2")
-	@Parameter(key = "result", type = ItemIO.OUTPUT)
 	public static final BiFunction<Double, Double, Double> subDoublesFunction = (t, u) -> t - u;
 
 	@OpField(names = MathOps.MUL)
-	@Parameter(key = "number1")
-	@Parameter(key = "number2")
-	@Parameter(key = "result", type = ItemIO.OUTPUT)
 	public static final BiFunction<Double, Double, Double> mulDoublesFunction = (t, u) -> t * u;
 
 	@OpField(names = MathOps.DIV)
-	@Parameter(key = "number1")
-	@Parameter(key = "number2")
-	@Parameter(key = "result", type = ItemIO.OUTPUT)
 	public static final BiFunction<Double, Double, Double> divDoublesFunction = (t, u) -> t / u;
 
 }

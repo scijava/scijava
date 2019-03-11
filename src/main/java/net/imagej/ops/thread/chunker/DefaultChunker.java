88dd0039e6679d901d3d63718cdb033dd41a501b
Gabriel Selzer <gabeselzer@gmail.com>
2019-03-11 15:06:52 -0500

    WIP: Migrate thread ops
    TODO: We need to port tests. However these tests require benchmark
    Also, We need to either inject ThreadService correctly or find another
    way to prevent NPEs from being thrown

---
+++ src/main/java/net/imagej/ops/thread/chunker/DefaultChunker.java
@@ -0,0 +1,101 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2014 - 2018 ImageJ developers.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package net.imagej.ops.thread.chunker;
+
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import org.scijava.log.LogService;
+import org.scijava.ops.core.Op;
+import org.scijava.ops.core.inplace.BiInplaceFirst;
+import org.scijava.param.Parameter;
+import org.scijava.plugin.Plugin;
+import org.scijava.struct.ItemIO;
+
+/**
+ * Simple default implementation of a Chunker Op. The list of
+ * elements is chunked into equally sized (besides the last one), disjoint
+ * chunks, which are processed in parallel. The stepSize is set to one, i.e.
+ * each chunk consists of consecutive elements.
+ * 
+ * @author Christian Dietz (University of Konstanz)
+ */
+@Plugin(type = Op.class, name = "thread.chunker")
+@Parameter(key = "chunkable", type = ItemIO.BOTH)
+@Parameter(key = "numberOfElements")
+public class DefaultChunker extends AbstractChunker implements BiInplaceFirst<Chunk, Long>{
+
+	private final int STEP_SIZE = 1;
+
+	@org.scijava.plugin.Parameter
+	public LogService logService;
+
+	@Override
+	public void mutate(final Chunk chunkable, final Long numberOfElements) {
+
+		// TODO: is there a better way to determine the optimal chunk size?
+		
+		final long numSteps = Math.max(1, 
+			numberOfElements / Runtime.getRuntime().availableProcessors()) ;
+		
+		final int numChunks = (int) (numberOfElements / numSteps);
+
+		final ArrayList<Future<?>> futures = new ArrayList<>(numChunks);
+
+		for (int i = 0; i < numChunks - 1; i++) {
+			final long j = i;
+
+			//TODO: ThreadService is not being filled, causes this to throw a NPE
+			futures.add(threadService.run(() -> chunkable.execute(j * numSteps, STEP_SIZE, numSteps)));
+		}
+
+		//TODO: ThreadService is not being filled, causes this to throw a NPE
+		// last chunk additionally add the rest of elements
+		futures.add(threadService.run(() -> chunkable.execute((numChunks - 1) * numSteps, STEP_SIZE,
+			(int) (numSteps + numberOfElements % numSteps))));
+
+		for (final Future<?> future : futures) {
+			try {
+				if (isCanceled()) {
+					break;
+				}
+				future.get();
+			}
+			catch (final InterruptedException exc) {
+				throw new RuntimeException(exc);
+			}
+			catch (final ExecutionException exc) {
+				throw new RuntimeException(exc);
+			}
+		}
+	}
+
+}

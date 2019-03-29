c844c20a437876b8ef735c89daec8a97e6e004e7
Gabriel Selzer <gabeselzer@gmail.com>
2019-03-29 15:53:02 -0500

    Fix a Lift that only computed the first pixel

--- src/main/java/org/scijava/ops/util/Maps.java
+++ src/main/java/org/scijava/ops/util/Maps.java
@@ -1,5 +1,7 @@
 package org.scijava.ops.util;
 
+import com.google.common.collect.Streams;
+
 import java.lang.reflect.Array;
 import java.util.Iterator;
 import java.util.List;
@@ -7,8 +9,6 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import org.scijava.ops.core.computer.Computer;
-
-import com.google.common.collect.Streams;
 
 public class Maps {
 
@@ -51,7 +51,9 @@ public class Maps {
 				return (iter1, iter2) -> {
 					Iterator<I> i1 = iter1.iterator();
 					Iterator<O> i2 = iter2.iterator();
-					computer.compute(i1.next(), i2.next());
+					while(i1.hasNext() && i2.hasNext()) {
+						computer.compute(i1.next(), i2.next());
+					}
 				};
 			}
 		}

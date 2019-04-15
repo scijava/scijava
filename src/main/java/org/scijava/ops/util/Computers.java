b933f0071bb35e4cf50af2a15c058f10ff6a4be9
Gabriel Selzer <gabeselzer@gmail.com>
2019-04-15 15:04:04 -0500

    Add Op creation helpers

--- src/main/java/org/scijava/ops/util/Computers.java
+++ src/main/java/org/scijava/ops/util/Computers.java
@@ -6,6 +6,10 @@ import org.scijava.ops.OpService;
 import org.scijava.ops.core.Op;
 import org.scijava.ops.core.computer.BiComputer;
 import org.scijava.ops.core.computer.Computer;
+import org.scijava.ops.core.computer.Computer3;
+import org.scijava.ops.core.computer.Computer4;
+import org.scijava.ops.core.computer.Computer5;
+import org.scijava.ops.core.computer.NullaryComputer;
 import org.scijava.ops.types.Nil;
 import org.scijava.util.Types;
 
@@ -18,8 +22,25 @@ public class Computers {
 		// NB: Prevent instantiation of utility class.
 	}
 
-	public static <I, O> Computer<I, O> unary(final OpService ops, final String opName,
-			final Nil<I> inputType, final Nil<O> outputType, final Object... secondaryArgs) {
+	public static <O> NullaryComputer<O> nullary(final OpService ops, final String opName, final Nil<O> outputType,
+			final Object... secondaryArgs) {
+		Nil<NullaryComputer<O>> computerNil = new Nil<NullaryComputer<O>>() {
+			@Override
+			public Type getType() {
+				return Types.parameterize(NullaryComputer.class, new Type[] { outputType.getType() });
+			}
+		};
+
+		return ops.findOp( //
+				opName, //
+				computerNil, //
+				new Nil[] { outputType }, //
+				new Nil[] { outputType }, //
+				secondaryArgs);
+	}
+
+	public static <I, O> Computer<I, O> unary(final OpService ops, final String opName, final Nil<I> inputType,
+			final Nil<O> outputType, final Object... secondaryArgs) {
 
 		Nil<Computer<I, O>> computerNil = new Nil<Computer<I, O>>() {
 			@Override
@@ -52,6 +73,67 @@ public class Computers {
 				opName, //
 				computerNil, //
 				new Nil[] { input1Type, input2Type, outputType }, //
+				new Nil[] { outputType }, //
+				secondaryArgs);
+	}
+
+	public static <I1, I2, I3, O> Computer3<I1, I2, I3, O> ternary(final OpService ops, final String opName,
+			final Nil<I1> input1Type, final Nil<I2> input2Type, final Nil<I3> input3Type, final Nil<O> outputType,
+			final Object... secondaryArgs) {
+
+		Nil<Computer3<I1, I2, I3, O>> computerNil = new Nil<Computer3<I1, I2, I3, O>>() {
+			@Override
+			public Type getType() {
+				return Types.parameterize(Computer3.class, new Type[] { input1Type.getType(), input2Type.getType(),
+						input3Type.getType(), outputType.getType() });
+			}
+		};
+
+		return ops.findOp( //
+				opName, //
+				computerNil, //
+				new Nil[] { input1Type, input2Type, input3Type, outputType }, //
+				new Nil[] { outputType }, //
+				secondaryArgs);
+	}
+
+	public static <I1, I2, I3, I4, O> Computer4<I1, I2, I3, I4, O> quaternary(final OpService ops, final String opName,
+			final Nil<I1> input1Type, final Nil<I2> input2Type, final Nil<I3> input3Type, final Nil<I4> input4Type,
+			final Nil<O> outputType, final Object... secondaryArgs) {
+
+		Nil<Computer4<I1, I2, I3, I4, O>> computerNil = new Nil<Computer4<I1, I2, I3, I4, O>>() {
+			@Override
+			public Type getType() {
+				return Types.parameterize(Computer4.class, new Type[] { input1Type.getType(), input2Type.getType(),
+						input3Type.getType(), input4Type.getType(), outputType.getType() });
+			}
+		};
+
+		return ops.findOp( //
+				opName, //
+				computerNil, //
+				new Nil[] { input1Type, input2Type, input3Type, input4Type, outputType }, //
+				new Nil[] { outputType }, //
+				secondaryArgs);
+	}
+
+	public static <I1, I2, I3, I4, I5, O> Computer5<I1, I2, I3, I4, I5, O> quinary(final OpService ops,
+			final String opName, final Nil<I1> input1Type, final Nil<I2> input2Type, final Nil<I3> input3Type,
+			final Nil<I4> input4Type, final Nil<I5> input5Type, final Nil<O> outputType,
+			final Object... secondaryArgs) {
+
+		Nil<Computer5<I1, I2, I3, I4, I5, O>> computerNil = new Nil<Computer5<I1, I2, I3, I4, I5, O>>() {
+			@Override
+			public Type getType() {
+				return Types.parameterize(Computer5.class, new Type[] { input1Type.getType(), input2Type.getType(),
+						input3Type.getType(), input4Type.getType(), input5Type.getType(), outputType.getType() });
+			}
+		};
+
+		return ops.findOp( //
+				opName, //
+				computerNil, //
+				new Nil[] { input1Type, input2Type, input3Type, input4Type, input5Type, outputType }, //
 				new Nil[] { outputType }, //
 				secondaryArgs);
 	}
--- src/main/java/org/scijava/ops/util/Functions.java
+++ src/main/java/org/scijava/ops/util/Functions.java
@@ -8,6 +8,7 @@ import org.scijava.ops.OpService;
 import org.scijava.ops.core.Op;
 import org.scijava.ops.core.function.Function3;
 import org.scijava.ops.core.function.Function4;
+import org.scijava.ops.core.function.Function5;
 import org.scijava.ops.types.Nil;
 import org.scijava.util.Types;
 
@@ -94,6 +95,26 @@ public class Functions {
 				opName, //
 				functionNil, //
 				new Nil[] { input1Type, input2Type, input3Type, input4Type }, //
+				new Nil[] { outputType }, //
+				secondaryArgs);
+	}
+	
+	public static <I1, I2, I3, I4, I5, O> Function5<I1, I2, I3, I4, I5, O> quinary(final OpService ops,
+			final String opName, final Nil<I1> input1Type, final Nil<I2> input2Type, final Nil<I3> input3Type,
+			final Nil<I4> input4Type, final Nil<I5> input5Type, final Nil<O> outputType, final Object... secondaryArgs) {
+
+		Nil<Function5<I1, I2, I3, I4, I5, O>> functionNil = new Nil<Function5<I1, I2, I3, I4, I5, O>>() {
+			@Override
+			public Type getType() {
+				return Types.parameterize(Function5.class, new Type[] { input1Type.getType(), input2Type.getType(),
+						input3Type.getType(), input4Type.getType(), input5Type.getType(), outputType.getType() });
+			}
+		};
+
+		return ops.findOp( //
+				opName, //
+				functionNil, //
+				new Nil[] { input1Type, input2Type, input3Type, input4Type, input5Type }, //
 				new Nil[] { outputType }, //
 				secondaryArgs);
 	}

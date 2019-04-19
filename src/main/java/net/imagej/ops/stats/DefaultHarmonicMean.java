a9f020ff51daddf2b303ac1d99ae4a7eee885ad1
Gabriel Selzer <gabeselzer@gmail.com>
2019-04-19 09:50:49 -0500

    Port stats namespace

---
+++ src/main/java/net/imagej/ops/stats/DefaultHarmonicMean.java
@@ -0,0 +1,78 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2014 - 2018 ImageJ developers.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package net.imagej.ops.stats;
+
+import net.imglib2.type.numeric.RealType;
+
+import org.scijava.ops.OpDependency;
+import org.scijava.ops.core.Op;
+import org.scijava.ops.core.computer.Computer;
+import org.scijava.param.Parameter;
+import org.scijava.plugin.Plugin;
+import org.scijava.struct.ItemIO;
+
+/**
+ * {@link Op} to calculate the {@code stats.harmonicMean} using
+ * {@code stats.size} and {@code stats.sumOfInverses}.
+ * 
+ * @author Daniel Seebacher (University of Konstanz)
+ * @author Christian Dietz (University of Konstanz)
+ * @param <I> input type
+ * @param <O> output type
+ */
+@Plugin(type = Op.class, name = "stats.harmonicMean")
+@Parameter(key = "iterableInput")
+@Parameter(key = "harmonicMean", type = ItemIO.BOTH)
+public class DefaultHarmonicMean<I extends RealType<I>, O extends RealType<O>>
+	implements Computer<Iterable<I>, O>
+{
+	
+	@OpDependency(name = "stats.size")
+	private Computer<Iterable<I>, O> sizeComputer;
+
+	@OpDependency(name = "stats.sumOfInverses")
+	private Computer<Iterable<I>, O> sumOfInversesComputer;
+
+	@Override
+	public void compute(final Iterable<I> input, final O output) {
+		final O area = output.createVariable();
+		sizeComputer.compute(input, area);
+		final O sumOfInverses = output.createVariable();
+		sumOfInversesComputer.compute(input, sumOfInverses);
+
+		if (sumOfInverses.getRealDouble() != 0) {
+			output.set(area);
+			output.div(sumOfInverses);
+		}
+		else {
+			output.setReal(0);
+		}
+	}
+}

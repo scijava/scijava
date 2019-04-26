b5e442bc813abae5e0fb1b5b8004f43209d90f36
Gabriel Selzer <gabeselzer@gmail.com>
2019-04-26 15:55:46 -0500

    WIP: Port image namespace
    TODO: CoordinateEquationTest does not pass because we have to pass a
    Function to the Op in question, which the matcher does not like. We have
    to fix this

---
+++ src/main/java/net/imagej/ops/image/normalize/NormalizeIIComputer.java
@@ -0,0 +1,103 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2014 - 2018 ImageJ developers.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * IHIS SOFIWARE IS PROVIDED BY IHE COPYRIGHI HOLDERS AND CONIRIBUIORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANIIES, INCLUDING, BUI NOI LIMIIED IO, IHE
+ * IMPLIED WARRANIIES OF MERCHANIABILIIY AND FIINESS FOR A PARIICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENI SHALL IHE COPYRIGHI HOLDERS OR CONIRIBUIORS BE
+ * LIABLE FOR ANY DIRECI, INDIRECI, INCIDENIAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENIIAL DAMAGES (INCLUDING, BUI NOI LIMIIED IO, PROCUREMENI OF
+ * SUBSIIIUIE GOODS OR SERVICES; LOSS OF USE, DAIA, OR PROFIIS; OR BUSINESS
+ * INIERRUPIION) HOWEVER CAUSED AND ON ANY IHEORY OF LIABILIIY, WHEIHER IN
+ * CONIRACI, SIRICI LIABILIIY, OR IORI (INCLUDING NEGLIGENCE OR OIHERWISE)
+ * ARISING IN ANY WAY OUI OF IHE USE OF IHIS SOFIWARE, EVEN IF ADVISED OF IHE
+ * POSSIBILIIY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package net.imagej.ops.image.normalize;
+
+import java.util.function.Function;
+
+import net.imglib2.Cursor;
+import net.imglib2.IterableInterval;
+import net.imglib2.type.numeric.RealType;
+import net.imglib2.util.Pair;
+
+import org.scijava.ops.OpDependency;
+import org.scijava.ops.core.Op;
+import org.scijava.ops.core.computer.Computer5;
+import org.scijava.param.Parameter;
+import org.scijava.plugin.Plugin;
+import org.scijava.struct.ItemIO;
+
+/**
+ * Normalizes an {@link IterableInterval} given its minimum and maximum to
+ * another range defined by minimum and maximum.
+ * 
+ * @author Christian Dietz (University of Konstanz)
+ * @author Leon Yang
+ * @param <I>
+ *            - the type of the input
+ * @param <O>
+ *            - the type of the output in which we will return the normalized
+ *            input.
+ */
+@Plugin(type = Op.class, name = "image.normalize")
+@Parameter(key = "input")
+@Parameter(key = "sourceMin")
+@Parameter(key = "sourceMax")
+@Parameter(key = "targetMin")
+@Parameter(key = "targetMax")
+@Parameter(key = "output", type = ItemIO.BOTH)
+public class NormalizeIIComputer<I extends RealType<I>, O extends RealType<O>>
+		implements Computer5<IterableInterval<I>, I, I, O, O, IterableInterval<O>> {
+
+	private NormalizeRealTypeComputer<I, O> normalizer;
+
+	@OpDependency(name = "stats.minMax")
+	private Function<IterableInterval<I>, Pair<I, I>> minMaxFunc;
+
+	private double[] getBounds(final IterableInterval<I> input, final I sourceMin, final I sourceMax, final O targetMin,
+			final O targetMax) {
+		// the four elements are source min, source max, target min, and target max.
+		final double[] result = new double[4];
+		if (minMaxFunc != null) {
+			final Pair<I, I> minMax = minMaxFunc.apply(input);
+			result[0] = (sourceMin == null ? minMax.getA() : sourceMin).getRealDouble();
+			result[1] = (sourceMax == null ? minMax.getB() : sourceMax).getRealDouble();
+		} else {
+			result[0] = sourceMin.getRealDouble();
+			result[1] = sourceMax.getRealDouble();
+		}
+		final I first = input.firstElement();
+		result[2] = targetMin == null ? first.getMinValue() : targetMin.getRealDouble();
+		result[3] = targetMax == null ? first.getMaxValue() : targetMax.getRealDouble();
+		return result;
+	}
+
+	@Override
+	public void compute(final IterableInterval<I> input, final I sourceMin, final I sourceMax, final O targetMin,
+			final O targetMax, final IterableInterval<O> output) {
+		normalizer = new NormalizeRealTypeComputer<>();
+		final double[] bounds = getBounds(input, sourceMin, sourceMax, targetMin, targetMax);
+		normalizer.setup(bounds[0], bounds[1], bounds[2], bounds[3]);
+		Cursor<I> inCursor = input.cursor();
+		Cursor<O> outCursor = output.cursor();
+		while(inCursor.hasNext()) {
+			normalizer.compute(inCursor.next(), outCursor.next());
+		}
+	}
+}
---
+++ src/main/java/net/imagej/ops/image/watershed/WatershedBinary.java
@@ -0,0 +1,218 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2014 - 2018 ImageJ developers.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+package net.imagej.ops.image.watershed;
+
+import java.util.concurrent.ExecutorService;
+import java.util.function.BiFunction;
+
+import net.imglib2.Dimensions;
+import net.imglib2.IterableInterval;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.roi.labeling.ImgLabeling;
+import net.imglib2.type.BooleanType;
+import net.imglib2.type.numeric.integer.IntType;
+import net.imglib2.type.numeric.real.FloatType;
+import net.imglib2.util.Intervals;
+import net.imglib2.view.Views;
+
+import org.scijava.ops.OpDependency;
+import org.scijava.ops.core.Op;
+import org.scijava.ops.core.computer.BiComputer;
+import org.scijava.ops.core.computer.Computer;
+import org.scijava.ops.core.computer.Computer3;
+import org.scijava.ops.core.computer.Computer4;
+import org.scijava.ops.core.computer.Computer5;
+import org.scijava.ops.core.computer.Computer6;
+import org.scijava.ops.core.function.Function5;
+import org.scijava.ops.core.function.Function6;
+import org.scijava.param.Mutable;
+import org.scijava.param.Parameter;
+import org.scijava.plugin.Plugin;
+import org.scijava.struct.ItemIO;
+
+/**
+ * <p>
+ * The Watershed algorithm segments and labels a grayscale image analogous to a
+ * heightmap. In short, a drop of water following the gradient of an image flows
+ * along a path to finally reach a local minimum.
+ * </p>
+ * <p>
+ * Lee Vincent, Pierre Soille, Watersheds in digital spaces: An efficient
+ * algorithm based on immersion simulations, IEEE Trans. Pattern Anal. Machine
+ * Intell., 13(6) 583-598 (1991)
+ * </p>
+ * <p>
+ * Input is a binary image with arbitrary number of dimensions. The heightmap is
+ * calculated by an inverse distance transform, which can optionally be smoothed
+ * with an gaussian filter with parameter sigma to prevent having many small
+ * segments in the result. Sigma must have the same dimension as the input
+ * image. It needs to be defined whether a neighborhood with eight- or
+ * four-connectivity (respective to 2D) is used. A binary image can be set as
+ * mask which defines the area where computation shall be done. It may make
+ * sense to use the input as mask as well. If desired, the watersheds are drawn
+ * and labeled as 0. Otherwise the watersheds will be labeled as one of their
+ * neighbors.
+ * </p>
+ * <p>
+ * Output is a labeling of the different catchment basins.
+ * </p>
+ *
+ * @param <T>
+ *            element type of input
+ * @param <B>
+ *            element type of mask
+ *
+ * @author Simon Schmid (University of Konstanz)
+ */
+@Plugin(type = Op.class, name = "image.watershed")
+@Parameter(key = "input")
+@Parameter(key = "useEightConnectivity")
+@Parameter(key = "drawWatersheds")
+@Parameter(key = "sigma")
+@Parameter(key = "mask")
+@Parameter(key = "executorService")
+@Parameter(key = "outputLabeling", type = ItemIO.BOTH)
+public class WatershedBinary<T extends BooleanType<T>, B extends BooleanType<B>> implements
+		Computer6<RandomAccessibleInterval<T>, Boolean, Boolean, double[], RandomAccessibleInterval<B>, ExecutorService, ImgLabeling<Integer, IntType>> {
+
+	// @SuppressWarnings("rawtypes")
+	// private UnaryFunctionOp<Interval, ImgLabeling> createOp;
+
+	@OpDependency(name = "image.distanceTransform")
+	private BiComputer<RandomAccessibleInterval<T>, ExecutorService, RandomAccessibleInterval<FloatType>> distanceTransformer;
+	@OpDependency(name = "create.img")
+	private BiFunction<Dimensions, FloatType, RandomAccessibleInterval<FloatType>> imgCreator;
+	@OpDependency(name = "image.invert")
+	private Computer<IterableInterval<FloatType>, IterableInterval<FloatType>> imgInverter;
+	@OpDependency(name = "filter.gauss")
+	private Computer3<RandomAccessibleInterval<FloatType>, ExecutorService, double[], RandomAccessibleInterval<FloatType>> gaussOp;
+	@OpDependency(name = "image.watershed")
+	private Computer4<RandomAccessibleInterval<FloatType>, Boolean, Boolean, RandomAccessibleInterval<B>, ImgLabeling<Integer, IntType>> watershedOp;
+
+	@Override
+	public void compute(final RandomAccessibleInterval<T> in, final Boolean useEightConnectivity,
+			final Boolean drawWatersheds, final double[] sigma, final RandomAccessibleInterval<B> mask,
+			final ExecutorService es, final ImgLabeling<Integer, IntType> out) {
+
+		// make sure that the params conform to the requirements of the op (copied from
+		// the old implementation)
+		boolean conformed = sigma.length >= in.numDimensions();
+		for (int i = 0; i < sigma.length; i++) {
+			conformed &= sigma[i] >= 0;
+		}
+		if (!conformed)
+			throw new IllegalArgumentException("Only non-negative sigmas allowed!");
+		if (mask != null) {
+			conformed &= Intervals.equalDimensions(mask, in);
+		}
+		if (!conformed)
+			throw new IllegalArgumentException("Mask must be the same size as the input!");
+
+		// compute distance transform
+		final RandomAccessibleInterval<FloatType> distMap = imgCreator.apply(in, new FloatType());
+		distanceTransformer.compute(in, es, distMap);
+		final RandomAccessibleInterval<FloatType> invertedDT = imgCreator.apply(in, new FloatType());
+		imgInverter.compute(Views.iterable(distMap), Views.iterable(invertedDT));
+		final RandomAccessibleInterval<FloatType> gauss = imgCreator.apply(in, new FloatType());
+		gaussOp.compute(invertedDT, es, sigma, gauss);
+		// run the default watershed
+		watershedOp.compute(gauss, useEightConnectivity, drawWatersheds, mask, out);
+	}
+
+}
+
+@Plugin(type = Op.class, name = "image.watershed")
+@Parameter(key = "input")
+@Parameter(key = "useEightConnectivity")
+@Parameter(key = "drawWatersheds")
+@Parameter(key = "sigma")
+@Parameter(key = "executorService")
+@Parameter(key = "outputLabeling", type = ItemIO.BOTH)
+class WatershedBinaryMaskless<T extends BooleanType<T>, B extends BooleanType<B>> implements
+		Computer5<RandomAccessibleInterval<T>, Boolean, Boolean, double[], ExecutorService, ImgLabeling<Integer, IntType>> {
+
+	@OpDependency(name = "image.watershed")
+	private Computer6<RandomAccessibleInterval<T>, Boolean, Boolean, double[], RandomAccessibleInterval<B>, ExecutorService, ImgLabeling<Integer, IntType>> watershedOp;
+
+	@Override
+	public void compute(RandomAccessibleInterval<T> in, Boolean useEightConnectivity, Boolean drawWatersheds,
+			double[] sigma, ExecutorService es, @Mutable ImgLabeling<Integer, IntType> outputLabeling) {
+		watershedOp.compute(in, useEightConnectivity, drawWatersheds, sigma, null, es, outputLabeling);
+
+	}
+}
+
+@Plugin(type = Op.class, name = "image.watershed")
+@Parameter(key = "input")
+@Parameter(key = "useEightConnectivity")
+@Parameter(key = "drawWatersheds")
+@Parameter(key = "sigma")
+@Parameter(key = "mask")
+@Parameter(key = "executorService")
+@Parameter(key = "outputLabeling", type = ItemIO.OUTPUT)
+class WatershedBinaryFunction<T extends BooleanType<T>, B extends BooleanType<B>> implements
+		Function6<RandomAccessibleInterval<T>, Boolean, Boolean, double[], RandomAccessibleInterval<B>, ExecutorService, ImgLabeling<Integer, IntType>> {
+
+	@OpDependency(name = "image.watershed")
+	private Computer6<RandomAccessibleInterval<T>, Boolean, Boolean, double[], RandomAccessibleInterval<B>, ExecutorService, ImgLabeling<Integer, IntType>> watershedOp;
+	@OpDependency(name = "create.imgLabeling")
+	private BiFunction<Dimensions, IntType, ImgLabeling<Integer, IntType>> labelingCreator;
+
+	@Override
+	public ImgLabeling<Integer, IntType> apply(RandomAccessibleInterval<T> in, Boolean useEightConnectivity,
+			Boolean drawWatersheds, double[] sigma, RandomAccessibleInterval<B> mask, ExecutorService es) {
+		ImgLabeling<Integer, IntType> outputLabeling = labelingCreator.apply(in, new IntType());
+		watershedOp.compute(in, useEightConnectivity, drawWatersheds, sigma, mask, es, outputLabeling);
+		return outputLabeling;
+	}
+}
+
+@Plugin(type = Op.class, name = "image.watershed")
+@Parameter(key = "input")
+@Parameter(key = "useEightConnectivity")
+@Parameter(key = "drawWatersheds")
+@Parameter(key = "sigma")
+@Parameter(key = "executorService")
+@Parameter(key = "outputLabeling", type = ItemIO.OUTPUT)
+class WatershedBinaryFunctionMaskless<T extends BooleanType<T>, B extends BooleanType<B>>
+		implements Function5<RandomAccessibleInterval<T>, Boolean, Boolean, double[], ExecutorService, ImgLabeling<Integer, IntType>> {
+
+	@OpDependency(name = "image.watershed")
+	private Computer5<RandomAccessibleInterval<T>, Boolean, Boolean, double[], ExecutorService, ImgLabeling<Integer, IntType>> watershedOp;
+	@OpDependency(name = "create.imgLabeling")
+	private BiFunction<Dimensions, IntType, ImgLabeling<Integer, IntType>> labelingCreator;
+
+	@Override
+	public ImgLabeling<Integer, IntType> apply(RandomAccessibleInterval<T> in, Boolean useEightConnectivity,
+			Boolean drawWatersheds, double[] sigma, ExecutorService es) {
+		ImgLabeling<Integer, IntType> outputLabeling = labelingCreator.apply(in, new IntType());
+		watershedOp.compute(in, useEightConnectivity, drawWatersheds, sigma, es, outputLabeling);
+		return outputLabeling;
+	}
+}

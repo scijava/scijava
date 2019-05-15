2f16a0c87c240a3f1980f177727179f507fa7a40
Marcel Wiedenmann <marcel.wiedenmann@knime.com>
2019-05-15 16:23:58 -0500

    WIP/Discuss: Make numBins parameter optional in HistogramCreate

--- src/main/java/net/imagej/ops/image/histogram/HistogramCreate.java
+++ src/main/java/net/imagej/ops/image/histogram/HistogramCreate.java
@@ -6,13 +6,13 @@
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -49,15 +49,19 @@ import org.scijava.struct.ItemIO;
  */
 @Plugin(type = Op.class, name = "image.histogram")
 @Parameter(key = "iterable")
-@Parameter(key = "numBins")
+@Parameter(key = "numBins", required = false)
 @Parameter(key = "histogram", type = ItemIO.OUTPUT)
 public class HistogramCreate<T extends RealType<T>> implements BiFunction<Iterable<T>, Integer, Histogram1d<T>>{
+
+	public static final int DEFAULT_NUM_BINS = 256;
 
 	@OpDependency(name = "stats.minMax")
 	private Function<Iterable<T>, Pair<T, T>> minMaxFunc;
 
 	@Override
-	public Histogram1d<T> apply(final Iterable<T> input, final Integer numBins) {
+	public Histogram1d<T> apply(final Iterable<T> input, Integer numBins) {
+		if (numBins == null) numBins = DEFAULT_NUM_BINS;
+
 		final Pair<T, T> res = minMaxFunc.apply(input);
 
 		final Histogram1d<T> histogram1d = new Histogram1d<>(

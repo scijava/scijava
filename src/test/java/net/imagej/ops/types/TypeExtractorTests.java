10e63c23bee116e683b5f69d7021ea834b1d5c55
Gabriel Selzer <gabeselzer@gmail.com>
2019-06-21 16:04:22 -0500

    Improve OutOfBoundsFactory Type Extractors

---
+++ src/test/java/net/imagej/ops/types/TypeExtractorTests.java
@@ -0,0 +1,69 @@
+package net.imagej.ops.types;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+import net.imagej.ops.AbstractOpTest;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.img.Img;
+import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.outofbounds.OutOfBoundsConstantValueFactory;
+import net.imglib2.outofbounds.OutOfBoundsFactory;
+import net.imglib2.outofbounds.OutOfBoundsRandomValueFactory;
+import net.imglib2.type.numeric.integer.UnsignedByteType;
+
+import org.junit.Test;
+import org.scijava.ops.OpField;
+import org.scijava.ops.core.OpCollection;
+import org.scijava.ops.types.TypeExtractor;
+import org.scijava.param.Parameter;
+import org.scijava.plugin.Plugin;
+import org.scijava.struct.ItemIO;
+
+/**
+ * Tests various {@link TypeExtractor}s.
+ * 
+ * @author Gabriel Selzer
+ *
+ */
+@Plugin(type = OpCollection.class)
+public class TypeExtractorTests extends AbstractOpTest {
+
+	@OpField(names = "test.oobcvfTypeExtractor")
+	@Parameter(key = "oobf")
+	@Parameter(key = "output", type = ItemIO.OUTPUT)
+	public final Function<OutOfBoundsConstantValueFactory<UnsignedByteType, RandomAccessibleInterval<UnsignedByteType>>, String> func = (
+			oobf) -> "oobcvf";
+
+	@Test
+	public void testOutOfBoundsConstantValueFactoryTypeExtractors() {
+		OutOfBoundsFactory<UnsignedByteType, RandomAccessibleInterval<UnsignedByteType>> oobf = new OutOfBoundsConstantValueFactory<>(
+				new UnsignedByteType(5));
+
+		String output = (String) ops.run("test.oobcvfTypeExtractor", oobf);
+		// make sure that output matches the return from the Op above, specific to the
+		// type of OOBF we passed through.
+		assert output.equals("oobcvf");
+	}
+
+	// Test Op returns a string different from the one above
+	@OpField(names = "test.oobrvfTypeExtractor")
+	@Parameter(key = "oobf")
+	@Parameter(key = "input")
+	@Parameter(key = "output", type = ItemIO.OUTPUT)
+	public final BiFunction<OutOfBoundsRandomValueFactory<UnsignedByteType, RandomAccessibleInterval<UnsignedByteType>>, RandomAccessibleInterval<UnsignedByteType>, String> funcRandom = (
+			oobf, rai) -> "oobrvf";
+
+	@Test
+	public void testOutOfBoundsRandomValueFactoryTypeExtractors() {
+		OutOfBoundsFactory<UnsignedByteType, RandomAccessibleInterval<UnsignedByteType>> oobf = new OutOfBoundsRandomValueFactory<>(
+				new UnsignedByteType(7), 7, 7);
+
+		Img<UnsignedByteType> img = ArrayImgs.unsignedBytes(new long[] { 10, 10 });
+		String output = (String) ops.run("test.oobrvfTypeExtractor", oobf, img);
+		// make sure that output matches the return from the Op above, specific to the
+		// type of OOBF we passed through.
+		assert output.equals("oobrvf");
+	}
+
+}

471a201122d0a1abca904f9097879c917df5096f
Gabriel Selzer <gabeselzer@gmail.com>
2019-06-24 16:52:13 -0500

    WIP: Port convert ops

---
+++ src/main/java/net/imagej/ops/convert/RealTypeConverter.java
@@ -0,0 +1,66 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2014 - 2018 ImageJ developers.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package net.imagej.ops.convert;
+
+import net.imglib2.IterableInterval;
+import net.imglib2.type.numeric.RealType;
+
+import org.scijava.ops.core.computer.Computer;
+
+/**
+ * Base class for ops which convert between {@link RealType}s.
+ * 
+ * @author Martin Horn (University of Konstanz)
+ */
+public abstract class RealTypeConverter<I extends RealType<I>, O extends RealType<O>>
+	implements Computer<I, O>
+{
+
+	/**
+	 * Allows the convert pix operation to determine some parameters from the
+	 * concrete input and output types.
+	 */
+	@SuppressWarnings("unused")
+	public void checkInput(I inType, O outType) {
+		// NB: default implementation
+	}
+
+	/**
+	 * If the pixels to be converted stem from an {@link IterableInterval} some
+	 * additionally needed parameters (e.g. for normalization) can be calculated
+	 * here (hence, some heavier calculation might take place here). Might never
+	 * be called!
+	 */
+	@SuppressWarnings("unused")
+	public void checkInput(IterableInterval<I> in) {
+		// NB: default implementation
+	}
+
+}

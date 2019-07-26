e8d28801654c858537f63a963c42238532952842
Gabriel Selzer <gabeselzer@gmail.com>
2019-07-26 13:03:56 -0500

    WIP: port imageMoments namespace

---
+++ src/main/java/net/imagej/ops/imagemoments/centralmoments/IterableCentralMoment00.java
@@ -0,0 +1,73 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2014 - 2018 ImageJ developers.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package net.imagej.ops.imagemoments.centralmoments;
+
+import net.imagej.ops.imagemoments.AbstractImageMomentOp;
+import net.imglib2.Cursor;
+import net.imglib2.IterableInterval;
+import net.imglib2.type.numeric.RealType;
+
+import org.scijava.Priority;
+import org.scijava.ops.core.Op;
+import org.scijava.param.Parameter;
+import org.scijava.plugin.Plugin;
+import org.scijava.struct.ItemIO;
+
+/**
+ * {@link Op} to calculate the {@code imageMoments.centralMoment00} directly.
+ * 
+ * @author Daniel Seebacher (University of Konstanz)
+ * @author Christian Dietz (University of Konstanz)
+ * @param <I>
+ *            input type
+ * @param <O>
+ *            output type
+ */
+@Plugin(type = Op.class, name = "imageMoments.centralMoment00", label = "Image Moment: CentralMoment00", priority = Priority.VERY_HIGH)
+@Parameter(key = "input")
+@Parameter(key = "output", type = ItemIO.BOTH)
+public class IterableCentralMoment00<I extends RealType<I>, O extends RealType<O>>
+		implements AbstractImageMomentOp<I, O> {
+
+	@Override
+	public void computeMoment(final IterableInterval<I> input, final O output) {
+
+		double moment00 = 0;
+
+		final Cursor<I> cur = input.localizingCursor();
+		while (cur.hasNext()) {
+			cur.fwd();
+			double val = cur.get().getRealDouble();
+			moment00 += val;
+		}
+
+		output.setReal(moment00);
+	}
+}
---
+++ src/main/java/net/imagej/ops/imagemoments/hu/DefaultHuMoment1.java
@@ -0,0 +1,73 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2014 - 2018 ImageJ developers.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package net.imagej.ops.imagemoments.hu;
+
+import net.imagej.ops.imagemoments.AbstractImageMomentOp;
+import net.imglib2.IterableInterval;
+import net.imglib2.type.numeric.RealType;
+
+import org.scijava.ops.OpDependency;
+import org.scijava.ops.core.Op;
+import org.scijava.ops.core.computer.Computer;
+import org.scijava.param.Parameter;
+import org.scijava.plugin.Plugin;
+import org.scijava.struct.ItemIO;
+
+/**
+ * {@link Op} to calculate the {@code imageMoments.huMoment1}.
+ * 
+ * @author Daniel Seebacher (University of Konstanz)
+ * @author Christian Dietz (University of Konstanz)
+ * @param <I> input type
+ * @param <O> output type
+ */
+@Plugin(type = Op.class, name = "imageMoments.huMoment1", label = "Image Moment: HuMoment1")
+@Parameter(key = "input")
+@Parameter(key = "output", type = ItemIO.BOTH)
+public class DefaultHuMoment1<I extends RealType<I>, O extends RealType<O>>
+	implements AbstractImageMomentOp<I, O> 
+{
+
+	@OpDependency(name = "imageMoments.normalizedCentralMoment20")
+	private Computer<IterableInterval<I>, O> normalizedCentralMoment20Func;
+
+	@OpDependency(name = "imageMoments.normalizedCentralMoment02")
+	private Computer<IterableInterval<I>, O> normalizedCentralMoment02Func;
+
+	@Override
+	public void computeMoment(final IterableInterval<I> input, final O output) {
+		final O n20 = output.createVariable();
+		normalizedCentralMoment20Func.compute(input, n20);
+		final O n02 = output.createVariable();
+		normalizedCentralMoment02Func.compute(input, n02);
+
+		output.setReal(n20.getRealDouble() + n02.getRealDouble());
+	}
+}

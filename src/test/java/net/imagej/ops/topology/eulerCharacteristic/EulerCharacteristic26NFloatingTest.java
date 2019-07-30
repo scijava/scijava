74f2b1529bfa966634aa45417c3dbf2246fd2eb2
Gabriel Selzer <gabeselzer@gmail.com>
2019-07-30 12:18:37 -0500

    WIP: port topology namespace
    TODO: ensure tests pass

---
+++ src/test/java/net/imagej/ops/topology/eulerCharacteristic/EulerCharacteristic26NFloatingTest.java
@@ -0,0 +1,140 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2014 - 2018 ImageJ developers.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+package net.imagej.ops.topology.eulerCharacteristic;
+
+import static net.imagej.ops.topology.eulerCharacteristic.TestHelper.drawCube;
+import static org.junit.Assert.assertEquals;
+
+import net.imagej.ops.AbstractOpTest;
+import net.imglib2.RandomAccess;
+import net.imglib2.img.Img;
+import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.type.logic.BitType;
+import net.imglib2.type.numeric.real.DoubleType;
+
+import org.junit.Test;
+
+/**
+ * Tests for the {@link EulerCharacteristic26NFloating} class
+ *
+ * @author Richard Domander (Royal Veterinary College, London)
+ */
+public class EulerCharacteristic26NFloatingTest extends AbstractOpTest {
+    @Test(expected = IllegalArgumentException.class)
+    public void testConforms() throws AssertionError {
+        final Img<BitType> img = ArrayImgs.bits(3, 3);
+
+        ops.run("topology.eulerCharacteristic26NFloating",img);
+    }
+
+    /**
+     * Test with a single voxel (=solid cube) that floats in the middle of a 3x3x3 space
+     * <p>
+     * Here χ = β_0 - β_1 + β_2 = 1 - 0 + 0 = 1.<br>
+     * The formula χ = vertices - edges + faces for surfaces of polyhedra doesn't apply because the cuboid is solid.
+     * </p>
+     */
+    @Test
+    public void testCube() throws Exception {
+        final Img<BitType> img = drawCube(1, 1, 1, 1);
+
+        final DoubleType result = new DoubleType();
+        ops.run("topology.eulerCharacteristic26NFloating",img, result);
+
+        assertEquals("Euler characteristic (χ) is incorrect", 1.0, result.get(), 1e-12);
+    }
+
+    /**
+     * Test with a single voxel (=solid cube) in a 1x1x1 space
+     * <p>
+     * In this op result shouldn't differ from {@link #testCube} because space is zero extended
+     * </p>
+     */
+    @Test
+    public void testEdgeCube() throws Exception {
+        final Img<BitType> img = drawCube(1, 1, 1, 0);
+
+        final DoubleType result = new DoubleType();
+        ops.run("topology.eulerCharacteristic26NFloating",img, result);
+
+        assertEquals("Euler characteristic (χ) is incorrect", 1.0, result.get(), 1e-12);
+    }
+
+    /**
+     * Test with a cube that has a cavity inside
+     * <p>
+     * Here χ = β_0 - β_1 + β_2 = 1 - 0 + 1 = 2
+     * </p>
+     */
+    @Test
+    public void testHollowCube() throws Exception {
+        final Img<BitType> img = drawCube(3, 3, 3, 1);
+        final RandomAccess<BitType> access = img.randomAccess();
+
+        // Add cavity
+        access.setPosition(new long[]{2, 2, 2});
+        access.get().setZero();
+
+        final DoubleType result = new DoubleType();
+        ops.run("topology.eulerCharacteristic26NFloating",img, result);
+
+        assertEquals("Euler characteristic (χ) is incorrect", 2.0, result.get(), 1e-12);
+    }
+
+    /**
+     * Test with a cube that has a "handle"
+     * <p>
+     * Here χ = β_0 - β_1 + β_2 = 1 - 1 + 0 = 0
+     * </p>
+     */
+    @Test
+    public void testHandleCube() throws Exception {
+        final Img<BitType> cube = drawCube(9, 9, 9, 5);
+        final RandomAccess<BitType> access = cube.randomAccess();
+
+        // Draw a handle on the front xy-face of the cuboid
+        access.setPosition(9, 0);
+        access.setPosition(6, 1);
+        access.setPosition(4, 2);
+        access.get().setOne();
+        access.setPosition(3, 2);
+        access.get().setOne();
+        access.setPosition(7, 1);
+        access.get().setOne();
+        access.setPosition(8, 1);
+        access.get().setOne();
+        access.setPosition(4, 2);
+        access.get().setOne();
+
+        final DoubleType result = new DoubleType();
+        ops.run("topology.eulerCharacteristic26NFloating",cube, result);
+
+        assertEquals("Euler characteristic (χ) is incorrect", 0.0, result.get(), 1e-12);
+    }
+}
---
+++ src/test/java/net/imagej/ops/topology/eulerCharacteristic/EulerCorrectionTest.java
@@ -0,0 +1,121 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2014 - 2018 ImageJ developers.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+package net.imagej.ops.topology.eulerCharacteristic;
+
+import static net.imagej.ops.topology.eulerCharacteristic.TestHelper.drawCube;
+import static org.junit.Assert.assertEquals;
+
+import net.imagej.ops.AbstractOpTest;
+import net.imagej.ops.topology.eulerCharacteristic.EulerCorrection.Traverser;
+import net.imglib2.img.Img;
+import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.type.logic.BitType;
+import net.imglib2.type.numeric.real.DoubleType;
+
+import org.junit.Test;
+
+/**
+ * Tests {@link EulerCorrection}
+ *
+ * @author Richard Domander (Royal Veterinary College, London)
+ */
+public class EulerCorrectionTest extends AbstractOpTest {
+    @Test(expected = IllegalArgumentException.class)
+    public void testConforms() throws Exception {
+        final Img<BitType> img = ArrayImgs.bits(3, 3);
+
+        ops.run("topology.eulerCorrection", img);
+    }
+
+    @Test
+    public void testCube() throws Exception {
+        final Img<BitType> cube = drawCube(3, 3, 3, 1);
+        final Traverser<BitType> traverser = new Traverser<>(cube);
+
+        final int vertices = EulerCorrection.stackCorners(traverser);
+        assertEquals("Number of stack vertices is incorrect", 0, vertices);
+
+        final long edges = EulerCorrection.stackEdges(traverser);
+        assertEquals("Number stack edge voxels is incorrect", 0, edges);
+
+        final int faces = EulerCorrection.stackFaces(traverser);
+        assertEquals("Number stack face voxels is incorrect", 0, faces);
+
+        final long voxelEdgeIntersections = EulerCorrection.voxelEdgeIntersections(traverser);
+        assertEquals("Number intersections is incorrect", 0, voxelEdgeIntersections);
+
+        final long voxelFaceIntersections = EulerCorrection.voxelFaceIntersections(traverser);
+        assertEquals("Number intersections is incorrect", 0, voxelFaceIntersections);
+
+        final long voxelEdgeFaceIntersections = EulerCorrection.voxelEdgeFaceIntersections(traverser);
+        assertEquals("Number intersections is incorrect", 0, voxelEdgeFaceIntersections);
+
+        DoubleType result = new DoubleType();
+        ops.run("topology.eulerCorrection", cube, result);
+
+        assertEquals("Euler correction is incorrect", 0, result.get(), 1e-12);
+    }
+
+    @Test
+    public void testEdgeCube() throws Exception {
+        final int edges = 12;
+        final int cubeSize = 3;
+        final int edgeSize = cubeSize - 2;
+        final Img<BitType> cube = drawCube(cubeSize, cubeSize, cubeSize, 0);
+        final Traverser<BitType> traverser = new Traverser<>(cube);
+
+        final int vertices = EulerCorrection.stackCorners(traverser);
+        assertEquals("Number of stack vertices is incorrect", 8, vertices);
+
+        final long stackEdges = EulerCorrection.stackEdges(traverser);
+        assertEquals("Number stack edge voxels is incorrect", edges * edgeSize, stackEdges);
+
+        final int faces = EulerCorrection.stackFaces(traverser);
+        assertEquals("Number stack face voxels is incorrect", 6 * edgeSize * edgeSize, faces);
+
+        final long voxelEdgeIntersections = EulerCorrection.voxelEdgeIntersections(traverser);
+        // you can fit n - 1 2x1 edges on edges whose size is n
+        final long expectedVEIntersections = edges * (cubeSize - 1);
+        assertEquals("Number intersections is incorrect", expectedVEIntersections, voxelEdgeIntersections);
+
+        final long xyVFIntersections = (cubeSize + 1) * (cubeSize + 1);
+        final long yzVFIntersections = (cubeSize - 1) * (cubeSize + 1);
+        final long xzVFIntersections = (cubeSize - 1) * (cubeSize - 1);
+        final long expectedVFIntersections = xyVFIntersections * 2 + yzVFIntersections * 2 + xzVFIntersections * 2;
+        final long voxelFaceIntersections = EulerCorrection.voxelFaceIntersections(traverser);
+        assertEquals("Number intersections is incorrect", expectedVFIntersections, voxelFaceIntersections);
+
+        final long voxelEdgeFaceIntersections = EulerCorrection.voxelEdgeFaceIntersections(traverser);
+        assertEquals("Number intersections is incorrect", 108, voxelEdgeFaceIntersections);
+
+        DoubleType result = new DoubleType();
+        ops.run("topology.eulerCorrection", cube, result);
+        assertEquals("Euler contribution is incorrect", 1, result.get(), 1e-12);
+    }
+}

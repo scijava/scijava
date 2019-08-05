4defaf5f11c3cfc1bdf9f6e1afdd54f2885338f1
Gabriel Selzer <gabeselzer@gmail.com>
2019-08-05 13:03:12 -0500

    Create more Op type interfaces
    Signed-off-by: Curtis Rueden <ctrueden@wisc.edu>

---
+++ src/main/java/org/scijava/ops/core/computer/Computer13.java
@@ -0,0 +1,14 @@
+package org.scijava.ops.core.computer;
+
+import org.scijava.ops.core.Consumer14;
+import org.scijava.param.Mutable;
+
+@FunctionalInterface
+public interface Computer13<I1, I2, I3, I4, I5, I6, I7, I8, I9, I10, I11, I12, I13, O> extends Consumer14<I1, I2, I3, I4, I5, I6, I7, I8, I9, I10, I11, I12, I13, O> {
+	void compute(I1 in1, I2 in2, I3 in3, I4 in4, I5 in5, I6 in6, I7 in7, I8 in8, I9 in9, I10 in10, I11 in11, I12 in12, I13 in13, @Mutable O out);
+
+	@Override
+	default void accept(I1 in1, I2 in2, I3 in3, I4 in4, I5 in5, I6 in6, I7 in7, I8 in8, I9 in9, I10 in10, I11 in11, I12 in12, I13 in13, O out) {
+		compute(in1, in2, in3, in4, in5, in6, in7, in8, in9, in10, in11, in12, in13, out);
+	}
+}

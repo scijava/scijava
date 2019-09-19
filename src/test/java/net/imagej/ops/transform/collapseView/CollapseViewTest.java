cb3709d4be9d430121935564ce884348f64a9c82
Gabriel Selzer <gabeselzer@gmail.com>
2019-09-19 16:02:13 -0500

    Migrate transform ops

---
+++ src/test/java/net/imagej/ops/transform/collapseView/CollapseViewTest.java
@@ -0,0 +1,95 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2014 - 2018 ImageJ developers.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+package net.imagej.ops.transform.collapseView;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.function.Function;
+
+import net.imglib2.RandomAccessible;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.img.Img;
+import net.imglib2.img.array.ArrayImgFactory;
+import net.imglib2.type.numeric.real.DoubleType;
+import net.imglib2.view.Views;
+import net.imglib2.view.composite.CompositeIntervalView;
+import net.imglib2.view.composite.CompositeView;
+import net.imglib2.view.composite.GenericComposite;
+
+import org.junit.Test;
+import org.scijava.ops.AbstractTestEnvironment;
+import org.scijava.ops.types.Nil;
+import org.scijava.ops.util.Functions;
+
+/**
+ * Tests {@link net.imagej.ops.Ops.Transform.CollapseView} ops.
+ * <p>
+ * This test only checks if the op call works with all parameters and that the
+ * result is equal to that of the {@link Views} method call. It is not a
+ * correctness test of {@link Views} itself.
+ * </p>
+ *
+ * @author Tim-Oliver Buchholz (University of Konstanz)
+ */
+public class CollapseViewTest extends AbstractTestEnvironment {
+
+	@Test
+	public void defaultCollapseTest() {
+		Img<DoubleType> img = new ArrayImgFactory<DoubleType>().create(new int[] { 10, 10 }, new DoubleType());
+
+		Function<RandomAccessibleInterval<DoubleType>, CompositeIntervalView<DoubleType, ? extends GenericComposite<DoubleType>>> collapseFunc = Functions
+				.unary(ops, "transform.collapseView", new Nil<RandomAccessibleInterval<DoubleType>>() {
+				}, new Nil<CompositeIntervalView<DoubleType, ? extends GenericComposite<DoubleType>>>() {
+				});
+
+		CompositeIntervalView<DoubleType, ? extends GenericComposite<DoubleType>> il2 = Views.collapse(img);
+		CompositeIntervalView<DoubleType, ? extends GenericComposite<DoubleType>> opr = collapseFunc.apply(img);
+
+		assertEquals(il2.numDimensions(), opr.numDimensions());
+	}
+
+	@Test
+	public void collapseRATest() {
+
+		Img<DoubleType> img = new ArrayImgFactory<DoubleType>().create(new int[] { 10, 10, 10 }, new DoubleType());
+
+		Function<RandomAccessible<DoubleType>, CompositeView<DoubleType, ? extends GenericComposite<DoubleType>>> collapseFunc = Functions
+				.unary(ops, "transform.collapseView", new Nil<RandomAccessible<DoubleType>>() {
+				}, new Nil<CompositeView<DoubleType, ? extends GenericComposite<DoubleType>>>() {
+				});
+
+		CompositeView<DoubleType, ? extends GenericComposite<DoubleType>> il2 = Views
+				.collapse((RandomAccessible<DoubleType>) img);
+
+		CompositeView<DoubleType, ? extends GenericComposite<DoubleType>> opr = collapseFunc.apply(img);
+
+		assertEquals(il2.numDimensions(), opr.numDimensions());
+	}
+
+}
---
+++ src/test/java/net/imagej/ops/transform/intervalView/IntervalViewTest.java
@@ -0,0 +1,121 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2014 - 2018 ImageJ developers.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+package net.imagej.ops.transform.intervalView;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.function.BiFunction;
+
+import net.imglib2.Cursor;
+import net.imglib2.Interval;
+import net.imglib2.RandomAccess;
+import net.imglib2.RandomAccessible;
+import net.imglib2.img.Img;
+import net.imglib2.img.array.ArrayImgFactory;
+import net.imglib2.type.numeric.real.DoubleType;
+import net.imglib2.view.IntervalView;
+import net.imglib2.view.Views;
+
+import org.junit.Test;
+import org.scijava.ops.AbstractTestEnvironment;
+import org.scijava.ops.core.function.Function3;
+import org.scijava.ops.types.Nil;
+import org.scijava.ops.util.Functions;
+import org.scijava.util.MersenneTwisterFast;
+
+/**
+ * Tests {@link net.imagej.ops.Ops.Transform.IntervalView} ops.
+ * <p>
+ * This test only checks if the op call works with all parameters and that the
+ * result is equal to that of the {@link Views} method call. It is not a
+ * correctness test of {@link Views} itself.
+ * </p>
+ *
+ * @author Tim-Oliver Buchholz (University of Konstanz)
+ */
+public class IntervalViewTest extends AbstractTestEnvironment {
+
+	private static final long SEED = 0x12345678;
+
+	@Test
+	public void defaultIntervalTest() {
+
+		BiFunction<RandomAccessible<DoubleType>, Interval, IntervalView<DoubleType>> intervalFunc = Functions
+				.binary(ops, "transform.intervalView", new Nil<RandomAccessible<DoubleType>>() {
+				}, new Nil<Interval>() {
+				}, new Nil<IntervalView<DoubleType>>() {
+				});
+
+		Img<DoubleType> img = new ArrayImgFactory<DoubleType>().create(new int[] { 10, 10 }, new DoubleType());
+
+		MersenneTwisterFast r = new MersenneTwisterFast(SEED);
+		for (DoubleType d : img) {
+			d.set(r.nextDouble());
+		}
+
+		Cursor<DoubleType> il2 = Views.interval(img, img).localizingCursor();
+		RandomAccess<DoubleType> opr = intervalFunc.apply(img, img).randomAccess();
+
+		while (il2.hasNext()) {
+			DoubleType e = il2.next();
+			opr.setPosition(il2);
+
+			assertEquals(e.get(), opr.get().get(), 1e-10);
+		}
+	}
+
+	@Test
+	public void intervalMinMaxTest() {
+
+		Function3<RandomAccessible<DoubleType>, long[], long[], IntervalView<DoubleType>> intervalFunc = Functions
+				.ternary(ops, "transform.intervalView", new Nil<RandomAccessible<DoubleType>>() {
+				}, new Nil<long[]>() {
+				}, new Nil<long[]>() {
+				}, new Nil<IntervalView<DoubleType>>() {
+				});
+
+		Img<DoubleType> img = new ArrayImgFactory<DoubleType>().create(new int[] { 10, 10 }, new DoubleType());
+
+		MersenneTwisterFast r = new MersenneTwisterFast(SEED);
+		for (DoubleType d : img) {
+			d.set(r.nextDouble());
+		}
+
+		Cursor<DoubleType> il2 = Views.interval(img, new long[] { 1, 1 }, new long[] { 8, 9 }).localizingCursor();
+		RandomAccess<DoubleType> opr = intervalFunc.apply(img, new long[] { 1, 1 }, new long[] { 8, 9 })
+				.randomAccess();
+
+		while (il2.hasNext()) {
+			DoubleType e = il2.next();
+			opr.setPosition(il2);
+
+			assertEquals(e.get(), opr.get().get(), 1e-10);
+		}
+	}
+}

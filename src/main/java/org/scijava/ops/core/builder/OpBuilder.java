07ecc484a4adc07724733f9c57fcf42559cea67a
Curtis Rueden <ctrueden@wisc.edu>
2019-09-23 00:51:18 +0200

    Implement builder pattern proof of concept
    It works for 0-ary, 1-ary and 2-ary computers and functions.
    
    Still need to add inplaces; the API may evolve in response.
    
    The next goal after that will be to switch to Velocity templates.

---
+++ src/main/java/org/scijava/ops/core/builder/OpBuilder.java
@@ -0,0 +1,492 @@
+/*
+ * #%L
+ * SciJava Operations: a framework for reusable algorithms.
+ * %%
+ * Copyright (C) 2016 - 2019 SciJava Ops developers.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package org.scijava.ops.core.builder;
+
+import java.lang.ref.WeakReference;
+import java.lang.reflect.Type;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+import org.scijava.ops.OpService;
+import org.scijava.ops.function.Computers;
+import org.scijava.ops.function.Computers.Arity0;
+import org.scijava.ops.function.Functions;
+import org.scijava.ops.function.Producer;
+import org.scijava.ops.types.Nil;
+import org.scijava.ops.types.TypeService;
+import org.scijava.util.Types;
+
+/**
+ * Convenience class for looking up and/or executing ops using a builder
+ * pattern.
+ * <p>
+ * TODO: Examples
+ * </p>
+ *
+ * @author Curtis Rueden
+ */
+public class OpBuilder {
+
+	private final OpService ops;
+	private final String opName;
+
+	public OpBuilder(final OpService ops, final String opName) {
+		this.ops = ops;
+		this.opName = opName;
+	}
+
+	/** Specifies the op accepts no inputs&mdash;i.e., a nullary op. */
+	public Arity0_OU input() {
+		return new Arity0_OU();
+	}
+
+	/** Specifies 1 input by value. */
+	public <I> Arity1_IV_OU<I> input(I in) {
+		return new Arity1_IV_OU<>(in);
+	}
+
+	/** Specifies 1 input by raw type. */
+	public <I> Arity1_IT_OU<I> inType(Class<I> inType) {
+		return inType(Nil.of(inType));
+	}
+
+	/** Specifies 1 input by generic type. */
+	public <I> Arity1_IT_OU<I> inType(Nil<I> inType) {
+		return new Arity1_IT_OU<>(inType);
+	}
+
+	/** Specifies 2 inputs by value. */
+	public <I1, I2> Arity2_IV_OU<I1, I2> input(I1 in1, I2 in2) {
+		return new Arity2_IV_OU<>(in1, in2);
+	}
+
+	/** Specifies 2 inputs by raw type. */
+	public <I1, I2> Arity2_IT_OU<I1, I2> inType(Class<I1> in1Type, Class<I2> in2Type) {
+		return inType(Nil.of(in1Type), Nil.of(in2Type));
+	}
+
+	/** Specifies 2 inputs by generic type. */
+	public <I1, I2> Arity2_IT_OU<I1, I2> inType(Nil<I1> in1Type, Nil<I2> in2Type) {
+		return new Arity2_IT_OU<>(in1Type, in2Type);
+		}
+
+	// -- Helper methods --
+
+	@SuppressWarnings({"unchecked"})
+	private <T> Nil<T> type(final WeakReference<T> obj) {
+		return (Nil<T>) Nil.of(ops.context().service(TypeService.class).reify(obj.get()));
+	}
+
+	// -- Helper classes --
+
+	/**
+	 * Builder with arity 0, output unspecified.
+	 *
+	 * @author Curtis Rueden
+	 */
+	public final class Arity0_OU {
+
+		public <O> Arity0_OV<O> output(final O out) {
+			return new Arity0_OV<>(out);
+		}
+
+		public <O> Arity0_OT<O> outType(final Class<O> outType) {
+			return outType(Nil.of(outType));
+		}
+
+		public <O> Arity0_OT<O> outType(final Nil<O> outType) {
+			return new Arity0_OT<>(outType);
+		}
+
+		public Producer<?> op() {
+			final Nil<Producer<Object>> specialType = new Nil<Producer<Object>>() {
+				@Override
+				public Type getType() {
+					return Types.parameterize(Producer.class, new Type[] {Object.class});
+				}
+			};
+			return ops.findOp(opName, specialType, new Nil<?>[0], Nil.of(Object.class));
+		}
+
+		public Object create() {
+			return op().create();
+		}
+	}
+
+	/**
+	 * Builder with arity 0, output type given.
+	 *
+	 * @author Curtis Rueden
+	 * @param <O> The type of the output.
+	 */
+	public final class Arity0_OT<O> {
+
+		private final Nil<O> outType;
+
+		public Arity0_OT(final Nil<O> outType) {
+			this.outType = outType;
+		}
+
+		public Producer<O> producer() {
+			final Nil<Producer<O>> specialType = new Nil<Producer<O>>() {
+				@Override
+				public Type getType() {
+					return Types.parameterize(Producer.class, new Type[] {outType.getType()});
+				}
+			};
+			return ops.findOp(opName, specialType, new Nil<?>[0], outType);
+		}
+
+		public Computers.Arity0<O> computer() {
+			return Computers.match(ops, opName, outType);
+		}
+
+		public O create() {
+			return producer().create();
+		}
+	}
+
+	/**
+	 * Builder with arity 0, output value given.
+	 *
+	 * @author Curtis Rueden
+	 * @param <O> The type of the output.
+	 */
+	public final class Arity0_OV<O> {
+
+		private final WeakReference<O> out;
+
+		public Arity0_OV(final O out) {
+			this.out = new WeakReference<>(out);
+		}
+
+		public Arity0<O> op() {
+			return Computers.match(ops, opName, type(out));
+		}
+
+		public void compute() {
+			op().compute(out.get());
+		}
+	}
+
+	/**
+	 * Builder with arity 1, input type given, output type given.
+	 *
+	 * @author Curtis Rueden
+	 * @param <I> The type of the input.
+	 * @param <O> The type of the output.
+	 */
+	public final class Arity1_IT_OT<I, O> {
+
+		private final Nil<I> inType;
+		private final Nil<O> outType;
+
+		public Arity1_IT_OT(final Nil<I> inType, final Nil<O> outType) {
+			this.inType = inType;
+			this.outType = outType;
+		}
+
+		public Function<I, O> function() {
+			return Functions.match(ops, opName, inType, outType);
+		}
+
+		public Computers.Arity1<I, O> computer() {
+			return Computers.match(ops, opName, inType, outType);
+		}
+	}
+
+	/**
+	 * Builder with arity 1, input type given, output unspecified.
+	 *
+	 * @author Curtis Rueden
+	 * @param <I> The type of the input.
+	 */
+	public final class Arity1_IT_OU<I> {
+
+		private final Nil<I> inType;
+
+		public Arity1_IT_OU(final Nil<I> inType) {
+			this.inType = inType;
+		}
+
+		public <O> Arity1_IT_OT<I, O> outType(final Class<O> outType) {
+			return outType(Nil.of(outType));
+		}
+
+		public <O> Arity1_IT_OT<I, O> outType(final Nil<O> outType) {
+			return new Arity1_IT_OT<>(inType, outType);
+		}
+
+		public Function<I, ?> op() {
+			return Functions.match(ops, opName, inType, Nil.of(Object.class));
+		}
+	}
+
+	/**
+	 * Builder with arity 1, input value given, output type given.
+	 *
+	 * @author Curtis Rueden
+	 * @param <I> The type of the input.
+	 * @param <O> The type of the output.
+	 */
+	public final class Arity1_IV_OT<I, O> {
+		
+		private final WeakReference<I> in;
+		private final Nil<O> outType;
+
+		public Arity1_IV_OT(final I in, final Nil<O> outType) {
+			this.in = new WeakReference<>(in);
+			this.outType = outType;
+		}
+
+		public Function<I, O> op() {
+			return Functions.match(ops, opName, type(in), outType);
+		}
+
+		public O apply() {
+			return op().apply(in.get());
+		}
+	}
+
+	/**
+	 * Builder with arity 1, input value given, output unspecified.
+	 *
+	 * @author Curtis Rueden
+	 * @param <I> The type of the input.
+	 */
+	public final class Arity1_IV_OU<I> {
+
+		private final WeakReference<I> in;
+
+		public Arity1_IV_OU(final I in) {
+			this.in = new WeakReference<>(in);
+		}
+
+		public <O> Arity1_IV_OV<I, O> output(final O out) {
+			return new Arity1_IV_OV<>(in.get(), out);
+		}
+
+		public <O> Arity1_IV_OT<I, O> outType(final Class<O> outType) {
+			return outType(Nil.of(outType));
+		}
+
+		public <O> Arity1_IV_OT<I, O> outType(final Nil<O> outType) {
+			return new Arity1_IV_OT<>(in.get(), outType);
+		}
+
+		public Function<I, ?> op() {
+			return Functions.match(ops, opName, type(in), Nil.of(Object.class));
+		}
+
+		public Object apply() {
+			return op().apply(in.get());
+		}
+	}
+
+	/**
+	 * Builder with arity 1, input value given, output value given.
+	 *
+	 * @author Curtis Rueden
+	 * @param <I> The type of the input.
+	 */
+	public final class Arity1_IV_OV<I, O> {
+
+		private final WeakReference<I> in;
+		private final WeakReference<O> out;
+
+		public Arity1_IV_OV(final I in, final O out) {
+			this.in = new WeakReference<>(in);
+			this.out = new WeakReference<>(out);
+		}
+
+		public Computers.Arity1<I, O> op() {
+			return Computers.match(ops, opName, type(in), type(out));
+		}
+
+		public void compute() {
+			op().compute(in.get(), out.get());
+		}
+	}
+
+	/**
+	 * Builder with arity 2, input types given, output type given.
+	 *
+	 * @author Curtis Rueden
+	 * @param <I1> The type of input 1.
+	 * @param <I2> The type of input 2.
+	 * @param <O> The type of the output.
+	 */
+	public final class Arity2_IT_OT<I1, I2, O> {
+		
+		private final Nil<I1> in1Type;
+		private final Nil<I2> in2Type;
+		private final Nil<O> outType;
+
+		public Arity2_IT_OT(final Nil<I1> in1Type, final Nil<I2> in2Type, final Nil<O> outType) {
+			this.in1Type = in1Type;
+			this.in2Type = in2Type;
+			this.outType = outType;
+		}
+
+		public BiFunction<I1, I2, O> function() {
+			return Functions.match(ops, opName, in1Type, in2Type, outType);
+		}
+
+		public Computers.Arity2<I1, I2, O> computer() {
+			return Computers.match(ops, opName, in1Type, in2Type, outType);
+		}
+	}
+
+	/**
+	 * Builder with arity 2, input types given, output unspecified.
+	 *
+	 * @author Curtis Rueden
+	 * @param <I1> The type of input 1.
+	 * @param <I2> The type of input 2.
+	 */
+	public final class Arity2_IT_OU<I1, I2> {
+
+		private final Nil<I1> in1Type;
+		private final Nil<I2> in2Type;
+
+		public Arity2_IT_OU(final Nil<I1> in1Type, final Nil<I2> in2Type) {
+			this.in1Type = in1Type;
+			this.in2Type = in2Type;
+		}
+
+		public <O> Arity2_IT_OT<I1, I2, O> outType(final Class<O> outType) {
+			return outType(Nil.of(outType));
+		}
+
+		public <O> Arity2_IT_OT<I1, I2, O> outType(final Nil<O> outType) {
+			return new Arity2_IT_OT<>(in1Type, in2Type, outType);
+		}
+
+		public BiFunction<I1, I2, ?> op() {
+			return Functions.match(ops, opName, in1Type, in2Type, Nil.of(Object.class));
+		}
+	}
+
+	/**
+	 * Builder with arity 2, input values given, output type given.
+	 *
+	 * @author Curtis Rueden
+	 * @param <I1> The type of input 1.
+	 * @param <I2> The type of input 2.
+	 * @param <O> The type of the output.
+	 */
+	public final class Arity2_IV_OT<I1, I2, O> {
+		
+		private final WeakReference<I1> in1;
+		private final WeakReference<I2> in2;
+		private final Nil<O> outType;
+
+		public Arity2_IV_OT(final I1 in1, final I2 in2, final Nil<O> outType) {
+			this.in1 = new WeakReference<>(in1);
+			this.in2 = new WeakReference<>(in2);
+			this.outType = outType;
+		}
+
+		public BiFunction<I1, I2, O> op() {
+			return Functions.match(ops, opName, type(in1), type(in2), outType);
+		}
+
+		public O apply() {
+			return op().apply(in1.get(), in2.get());
+		}
+	}
+
+	/**
+	 * Builder with arity 2, input values given, output unspecified.
+	 *
+	 * @author Curtis Rueden
+	 * @param <I1> The type of input 1.
+	 * @param <I2> The type of input 2.
+	 */
+	public final class Arity2_IV_OU<I1, I2> {
+
+		private final WeakReference<I1> in1;
+		private final WeakReference<I2> in2;
+
+		public Arity2_IV_OU(final I1 in1, final I2 in2) {
+			this.in1 = new WeakReference<>(in1);
+			this.in2 = new WeakReference<>(in2);
+		}
+
+		public <O> Arity2_IV_OV<I1, I2, O> output(final O out) {
+			return new Arity2_IV_OV<>(in1.get(), in2.get(), out);
+		}
+
+		public <O> Arity2_IV_OT<I1, I2, O> outType(final Class<O> outType) {
+			return outType(Nil.of(outType));
+		}
+
+		public <O> Arity2_IV_OT<I1, I2, O> outType(final Nil<O> outType) {
+			return new Arity2_IV_OT<>(in1.get(), in2.get(), outType);
+		}
+
+		public BiFunction<I1, I2, ?> op() {
+			return Functions.match(ops, opName, type(in1), type(in2), Nil.of(Object.class));
+		}
+
+		public Object apply() {
+			return op().apply(in1.get(), in2.get());
+		}
+	}
+
+	/**
+	 * Builder with arity 2, input values given, output value given.
+	 *
+	 * @author Curtis Rueden
+	 * @param <I1> The type of input 1.
+	 * @param <I2> The type of input 2.
+	 * @param <O> The type of the output.
+	 */
+	public final class Arity2_IV_OV<I1, I2, O> {
+
+		private final WeakReference<I1> in1;
+		private final WeakReference<I2> in2;
+		private final WeakReference<O> out;
+
+		public Arity2_IV_OV(final I1 in1, final I2 in2, final O out) {
+			this.in1 = new WeakReference<>(in1);
+			this.in2 = new WeakReference<>(in2);
+			this.out = new WeakReference<>(out);
+		}
+
+		public Computers.Arity2<I1, I2, O> op() {
+			return Computers.match(ops, opName, type(in1), type(in2), type(out));
+		}
+
+		public void compute() {
+			op().compute(in1.get(), in2.get(), out.get());
+		}
+	}
+}
---
+++ src/test/java/org/scijava/ops/core/builder/OpBuilderTest.java
@@ -0,0 +1,285 @@
+/*
+ * #%L
+ * SciJava Operations: a framework for reusable algorithms.
+ * %%
+ * Copyright (C) 2016 - 2019 SciJava Ops developers.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package org.scijava.ops.core.builder;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+import org.junit.Test;
+import org.scijava.ops.AbstractTestEnvironment;
+import org.scijava.ops.function.Computers;
+import org.scijava.ops.function.Producer;
+
+/**
+ * Tests {@link OpBuilder}.
+ * 
+ * @author Curtis Rueden
+ */
+public class OpBuilderTest extends AbstractTestEnvironment {
+
+	final double[] halves = new double[10];
+	{
+		for (int i = 0; i < halves.length; i++)
+			halves[i] = i / 2.;
+	}
+
+	// -- 0-ary --
+
+	/** Runs a nullary function in a vacuum. */
+	@Test
+	public void testArity0_OU_run() {
+		final Object result = name("test.helloWorld").input().create();
+		assertEquals("Hello, world!", result);
+	}
+
+	/** Runs a nullary function with a given output type. */
+	@Test
+	public void testArity0_OT_run() {
+		final String result = name("test.helloWorld").input().outType(String.class).create();
+		assertEquals("Hello, world!", result);
+	}
+
+	/** Runs a nullary computer with a given output value. */
+	@Test
+	public void testArity0_OV_run() {
+		final double[] result = new double[halves.length];
+		Arrays.fill(result, 12345);
+		name("math.zero").input().output(result).compute();
+		assertTrue(Arrays.stream(result).allMatch(v -> v == 0));
+	}
+
+	/** Matches a nullary function in a vacuum. */
+	@Test
+	public void testArity0_OU_match() {
+		final Producer<?> op = name("test.helloWorld").input().op();
+		final Object result = op.create();
+		assertEquals("Hello, world!", result);
+	}
+
+	/** Matches a nullary computer or function with a given output type. */
+	@Test
+	public void testArity0_OT_match() {
+		final Producer<String> op = //
+			name("test.helloWorld").input().outType(String.class).producer();
+		final String result = op.create();
+		assertEquals("Hello, world!", result);
+	}
+
+	/** Matches a nullary computer with a given output value. */
+	@Test
+	public void testArity0_OV_match() {
+		final double[] result = new double[10];
+		Arrays.fill(result, 12345);
+		final Computers.Arity0<double[]> op = //
+			name("math.zero").input().output(result).op();
+		op.compute(result);
+		assertTrue(Arrays.stream(result).allMatch(v -> v == 0));
+	}
+
+	// -- 1-ary --
+
+	/** Runs a unary function using input value only. */
+	@Test
+	public void testArity1_IV_OU_run() {
+		final Object result = name("math.sqrt").input(4.).apply();
+		assertEquals(2., result);
+	}
+
+	/** Runs a unary function using input value + output type. */
+	@Test
+	public void testArity1_IV_OT_run() {
+		final double result = name("math.sqrt").input(9.).outType(double.class).apply();
+		assertEquals(3., result, 0.);
+	}
+
+	/** Runs a unary computer using input value + output value. */
+	@Test
+	public void testArity1_IV_OV_run() {
+		final int[] result = new int[halves.length];
+		name("test.castToInt").input(halves).output(result).compute();
+		for (int i = 0; i < result.length; i++)
+			assertEquals((int) halves[i], result[i]);
+	}
+
+	/** Matches a unary function using input types only. */
+	@Test
+	public void testArity1_IT_OU_match() {
+		final Function<Double, ?> op = //
+			name("math.sqrt").inType(Double.class).op();
+		final Object result = op.apply(16.);
+		assertEquals(4., result);
+	}
+
+	/** Matches a unary computer using input types + output type. */
+	public void testArity1_IT_OT_matchC() {
+		final Computers.Arity1<double[], int[]> castToInt = //
+			name("test.castToInt").inType(double[].class).outType(int[].class).computer();
+		final int[] result = new int[halves.length];
+		castToInt.compute(halves, result);
+		for (int i = 0; i < result.length; i++)
+			assertEquals((int) halves[i], result[i]);
+	}
+
+	/** Matches a unary function using input types + output type. */
+	@Test
+	public void testArity1_IT_OT_matchF() {
+		final Function<Double, Double> op = //
+			name("math.sqrt").inType(Double.class).outType(Double.class).function();
+		final double result = op.apply(25.);
+		assertEquals(5., result, 0.);
+	}
+
+	/** Matches a unary function using input value only. */
+	@Test
+	public void testArity1_IV_OU_match() {
+		final Function<Double, ?> op = //
+			name("math.sqrt").input(36.).op();
+		final Object result = op.apply(49.);
+		assertEquals(7., result);
+	}
+
+	/** Matches a unary function using input value + output type. */
+	@Test
+	public void testArity1_IV_OT_match() {
+		final Function<Double, Double> op = //
+			name("math.sqrt").input(64.).outType(Double.class).op();
+		final double result = op.apply(81.);
+		assertEquals(9., result, 0.);
+	}
+
+	/** Matches a unary computer using input value + output value. */
+	@Test
+	public void testArity1_IV_OV_match() {
+		final int[] result = new int[halves.length];
+		final Computers.Arity1<double[], int[]> op = //
+			name("test.castToInt").input(halves).output(result).op();
+		op.compute(halves, result);
+		for (int i = 0; i < result.length; i++)
+			assertEquals((int) halves[i], result[i]);
+	}
+
+	// -- 2-ary --
+
+	/** Runs a binary function using input values only. */
+	@Test
+	public void testArity2_IV_OU_run() {
+		final Object result = name("math.add").input(1., 2.).apply();
+		assertEquals(3., result);
+	}
+
+	/** Runs a binary function using input values + output type. */
+	@Test
+	public void testArity2_IV_OT_run() {
+		final double result = name("math.add").input(3., 4.).outType(double.class).apply();
+		assertEquals(7., result, 0.);
+	}
+
+	/** Runs a binary computer using input values + output value. */
+	@Test
+	public void testArity2_IV_OV_run() {
+		final String[] s = {"quick", "brown", "fox"};
+		final Object[] o = {"er", "est", "es"};
+		final String[] result = new String[s.length];
+		name("test.concat").input(s, o).output(result).compute();
+		for (int i = 0; i < result.length; i++)
+			assertEquals(s[i] + o[i], result[i]);
+	}
+
+	/** Matches a binary function using input types only. */
+	@Test
+	public void testArity2_IT_OU_match() {
+		final BiFunction<Double, Double, ?> op = //
+			name("math.add").inType(Double.class, Double.class).op();
+		final Object result = op.apply(5., 6.);
+		assertEquals(11., result);
+	}
+
+	/** Matches a binary computer using input types + output type. */
+	@Test
+	public void testArity2_IT_OT_matchC() {
+		final Computers.Arity2<String[], Object[], String[]> op = //
+			name("test.concat").inType(String[].class, Object[].class).outType(String[].class).computer();
+		final String[] s = {"whirl", "p"};
+		final Object[] o = {"ed", "eas"};
+		final String[] result = new String[s.length];
+		op.compute(s, o, result);
+		for (int i = 0; i < result.length; i++)
+			assertEquals(s[i] + o[i], result[i]);
+	}
+
+	/** Matches a binary function using input types + output type. */
+	@Test
+	public void testArity2_IT_OT_matchF() {
+		final BiFunction<Double, Double, Double> op = //
+			name("math.add").inType(Double.class, Double.class).outType(Double.class).function();
+		final Object result = op.apply(7., 8.);
+		assertEquals(15., result);
+	}
+
+	/** Matches a binary function using input values only. */
+	@Test
+	public void testArity2_IV_OU_match() {
+		final BiFunction<Double, Double, ?> op = //
+			name("math.add").input(9., 10.).op();
+		final Object result = op.apply(11., 12.);
+		assertEquals(23., result);
+	}
+
+	/** Matches a binary function using input values + output type. */
+	@Test
+	public void testArity2_IV_OT_match() {
+		final BiFunction<Double, Double, Double> op = //
+			name("math.add").input(13., 14.).outType(Double.class).op();
+		final double result = op.apply(15., 16.);
+		assertEquals(31., result, 0.);
+	}
+
+	/** Matches a binary computer using input value + output value. */
+	@Test
+	public void testArity2_IV_OV_match() {
+		final String[] s = {"ups", "and", "downs"};
+		final Object[] o = {"ide", "rogynous", "tream"};
+		final String[] result = new String[s.length];
+		Computers.Arity2<String[], Object[], String[]> op = name("test.concat").input(s, o).output(result).op();
+		op.compute(s, o, result);
+		for (int i = 0; i < result.length; i++)
+			assertEquals(s[i] + o[i], result[i]);
+	}
+
+	// -- Helper methods --
+
+	private OpBuilder name(String opName) {
+		return new OpBuilder(ops, opName);
+	}
+}

65b1f4d833ad2418fc6b18111bcbc8bba503908b
Gabriel Selzer <gabeselzer@gmail.com>
2019-10-03 10:36:04 -0500

    autogenerate OpBuilder

--- src/test/java/org/scijava/ops/core/builder/OpBuilderTestOps.java
+++ src/test/java/org/scijava/ops/core/builder/OpBuilderTestOps.java
@@ -29,9 +29,14 @@
 
 package org.scijava.ops.core.builder;
 
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
 import org.scijava.ops.OpField;
 import org.scijava.ops.core.OpCollection;
 import org.scijava.ops.function.Computers;
+import org.scijava.ops.function.Functions;
+import org.scijava.ops.function.Inplaces;
 import org.scijava.ops.function.Producer;
 import org.scijava.plugin.Plugin;
 
@@ -43,16 +48,139 @@ import org.scijava.plugin.Plugin;
 @Plugin(type = OpCollection.class)
 public class OpBuilderTestOps {
 
-	@OpField(names = "test.helloWorld")
-	public final Producer<String> helloWorld = () -> "Hello, world!";
+	/*
+	 * -- FUNCTIONS --
+	 * 
+	 * The general procedure for these Ops: Given a set of inputs in1, in2, ...,
+	 * inN, The output will be sum(in1, in2, ..., inN).
+	 */
+
+	// ARITY ZERO
+	@OpField(names = "test.addDoubles")
+	public final Producer<Double> addDoubles0 = () -> 0.;
+
+	// ARITY 1
+	@OpField(names = "test.addDoubles")
+	public final Function<Double, Double> addDoubles1 = (in1) -> in1;
+
+	// ARITY 2
+	@OpField(names = "test.addDoubles")
+	public final BiFunction<Double, Double, Double> addDoubles2 = (in1, in2) -> in1 + in2;
+
+	// ARITY 3+ (CODE GENERATE THESE)
+	@OpField(names = "test.addDoubles")
+	public final Functions.Arity3<Double, Double, Double, Double> addDoubles3 = (in1, in2, in3) -> in1 + in2 + in3;
+
+	/*
+	 * -- INPLACES --
+	 * 
+	 * The general procedure for these Ops: Given a set of inputs in1, in2, ... ,
+	 * io, ..., inN, the output will be io = in1 * in2 * ... * io * ... * inN.
+	 * N.B. We do this in arrays since the doubles themselves are immutable. 
+	 */
+
+	// ARITY ONE
+	@OpField(names = "test.mulArrays")
+	public final Inplaces.Arity1<double[]> powDoubles1 = (io) -> {};
+
+	// ARITY TWO
+	@OpField(names = "test.mulArrays")
+	public final Inplaces.Arity2_1<double[], double[]> powDoubles2_1 = (io,
+			in2) -> {
+				for(int i = 0; i < io.length; i++) {
+					io[i] *= in2[i];
+				}
+			};
+
+	@OpField(names = "test.mulArrays")
+	public final Inplaces.Arity2_2<double[], double[]> powDoubles2_2 = (in1,
+			io) -> {
+				for(int i = 0; i < io.length; i++) {
+					io[i] *= in1[i];
+				}
+			};
+
+	// ARITY THREES
+	@OpField(names = "test.mulArrays")
+	public final Inplaces.Arity3_1<double[], double[], double[]> powDoubles3_1 = (io, in2,
+			in3) -> {
+				for(int i = 0; i < io.length; i++) {
+					io[i] *= in2[i];
+					io[i] *= in3[i];
+				}
+			};
+
+	@OpField(names = "test.mulArrays")
+	public final Inplaces.Arity3_2<double[], double[], double[]> powDoubles3_2 = (in1, io,
+			in3) -> {
+				for(int i = 0; i < io.length; i++) {
+					io[i] *= in1[i];
+					io[i] *= in3[i];
+				}
+			};
+
+	@OpField(names = "test.mulArrays")
+	public final Inplaces.Arity3_3<double[], double[], double[]> powDoubles3_3 = (in1, in2,
+			io) -> {
+				for(int i = 0; i < io.length; i++) {
+					io[i] *= in1[i];
+					io[i] *= in2[i];
+				}
+			};
+
+	/*
+	 * -- COMPUTERS --
+	 * 
+	 * The general procedure: given a set of inputs in1, in2, ... , inN, the output
+	 * is given as in1 * in2 * ... * inN. N.B. we use arrays here since the doubles
+	 * themselves are immutable
+	 */
+
+	// ARITY 0
+	@OpField(names = "test.addArrays")
+	public final Computers.Arity0<double[]> mulStrings0 = (out) -> {
+		for (int i = 0; i < out.length; i++)
+			out[i] = 0;
+	};
+
+	// ARITY 1
+	@OpField(names = "test.addArrays")
+	public final Computers.Arity1<double[], double[]> mulStrings1 = (in,
+			out) -> {
+				for(int i = 0; i < out.length; i++) {
+					out[i] = in[i];
+				}
+			};
+
+	// ARITY 2
+	@OpField(names = "test.addArrays")
+	public final Computers.Arity2<double[], double[], double[]> mulStrings2 = (in1, in2, 
+			out) -> {
+				for(int i = 0; i < out.length; i++) {
+					out[i] = in1[i] + in2[i];
+				}
+			};
+
+	// ARITY 3
+	@OpField(names = "test.addArrays")
+	public final Computers.Arity3<double[], double[], double[], double[]> mulStrings3 = (in1, in2, in3,
+			out) -> {
+				for(int i = 0; i < out.length; i++) {
+					out[i] = in1[i] + in2[i] + in3[i];
+				}
+			};
+
+	// TODO: delete
 
 	@OpField(names = "test.castToInt")
 	public final Computers.Arity1<double[], int[]> castToInt = (src, dest) -> {
-		for (int i=0; i<dest.length; i++) dest[i] = (int) src[i];
+		for (int i = 0; i < dest.length; i++)
+			dest[i] = (int) src[i];
 	};
 
 	@OpField(names = "test.concat")
-	public final Computers.Arity2<String[], Object[], String[]> concat = (s, o, out) -> { for (int i = 0; i < out.length; i++)
-		out[i] = s[i] + o[i];
+	public final Computers.Arity2<String[], Object[], String[]> concat = (s, o, out) -> {
+		for (int i = 0; i < out.length; i++)
+			out[i] = s[i] + o[i];
 	};
 }

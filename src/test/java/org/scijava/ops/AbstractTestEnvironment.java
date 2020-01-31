84fdc0c6c78dd1c4eb0957d5a7e43e6b9942c8e9
Curtis Rueden <ctrueden@wisc.edu>
2020-01-31 15:33:36 -0600

    Use assertIterationsEqual

--- src/test/java/org/scijava/ops/AbstractTestEnvironment.java
+++ src/test/java/org/scijava/ops/AbstractTestEnvironment.java
@@ -1,6 +1,11 @@
 package org.scijava.ops;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import java.util.Arrays;
+import java.util.Iterator;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -26,5 +31,17 @@ public abstract class AbstractTestEnvironment {
 	
 	protected static boolean arrayEquals(double[] arr1, Double... arr2) {
 		return Arrays.deepEquals(Arrays.stream(arr1).boxed().toArray(Double[]::new), arr2);
+	}
+
+	protected static <T> void assertIterationsEqual(final Iterable<T> expected,
+		final Iterable<T> actual)
+	{
+		final Iterator<T> e = expected.iterator();
+		final Iterator<T> a = actual.iterator();
+		while (e.hasNext()) {
+			assertTrue("Fewer elements than expected", a.hasNext());
+			assertEquals(e.next(), a.next());
+		}
+		assertFalse("More elements than expected", a.hasNext());
 	}
 }

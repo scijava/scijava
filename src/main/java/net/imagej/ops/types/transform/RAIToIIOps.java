5fc790f7a084c1dce4e49e754e63e9072d66fb4e
Gabriel Selzer <gjselzer@wisc.edu>
2020-02-21 14:27:44 -0600

    Update RAI->II Ops so that they are adaptors
    This only involved a name change

--- src/main/java/net/imagej/ops/types/transform/RAIToIIOps.java
+++ src/main/java/net/imagej/ops/types/transform/RAIToIIOps.java
@@ -13,12 +13,12 @@ import org.scijava.plugin.Plugin;
 @Plugin(type = OpCollection.class)
 public class RAIToIIOps<T, U, V> {
 
-	@OpField(names = "transform.raiToIterable", params = "op")
+	@OpField(names = "adapt", params = "op")
 	public final Function<Function<Iterable<T>, U>, Function<RandomAccessibleInterval<T>, U>> func = (in) -> {
 		return (in1) -> in.apply(Views.flatIterable(in1));
 	};
 
-	@OpField(names = "transform.raiToIterable", params = "op")
+	@OpField(names = "adapt", params = "op")
 	public final Function<BiFunction<Iterable<T>, Iterable<U>, V>, BiFunction<RandomAccessibleInterval<T>, RandomAccessibleInterval<U>, V>> biFunc = (in) -> {
 		return (in1, in2) -> in.apply(Views.flatIterable(in1), Views.flatIterable(in2));
 	};

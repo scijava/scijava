3a1c3b8b32c447186c85c1018055335fbae5ec43
Gabriel Selzer <gjselzer@wisc.edu>
2020-07-21 13:41:19 -0500

    Add assertCloseEnough method
    This allows the user to specify a "relative" epsilon instead of an
    absolute. This provides flexibility for a test class where different
    asserts require different epsilons; instead of providing a consistent
    epsilon, we instead provide a consistent number of decimal places to be
    checked.

---
+++ scijava/scijava-testutil/src/main/java/org/scijava/testutil/AssertClose.java
@@ -0,0 +1,62 @@
+/*
+ * #%L
+ * SciJava Operations: a framework for reusable algorithms.
+ * %%
+ * Copyright (C) 2018 SciJava developers.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package org.scijava.testutil;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Assertions;
+
+/**
+ * Assertions to be used when a relative delta should be used to compare the
+ * actual and expected values.
+ *
+ * @author Gabriel Selzer
+ * @see Assertions
+ */
+public class AssertClose {
+
+	/**
+	 * <em>Assert</em> that {@code expected} and {@code actual} are equal within
+	 * the non-negative delta {@code 10^epsilonExp}.
+	 * 
+	 * @param expected
+	 * @param actual
+	 * @param epsilonExp
+	 */
+	public static void asesrtCloseEnough(double expected, double actual,
+		int epsilonExp)
+	{
+		double numerator = Math.abs(expected);
+		double denominator = Math.pow(10., epsilonExp);
+		double epsilon = numerator / denominator;
+		assertEquals(expected, actual, epsilon);
+	}
+
+}

0b231d8769aaf8c1a535a4b6194ee150c25ff764
Curtis Rueden <ctrueden@wisc.edu>
2021-02-19 22:27:17 -0600

    Mutable: improve javadoc
    Explain what mutable means, and link to the Inplaces interfaces,
    which are the ones making use of this annotation.

--- scijava/scijava-ops/src/main/java/org/scijava/param/Mutable.java
+++ scijava/scijava-ops/src/main/java/org/scijava/param/Mutable.java
@@ -5,7 +5,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**Annotation to mark a method parameter as mutable.  */
+/**
+ * Annotation to mark a parameter as mutable: the value of the given argument
+ * will be altered by the algorithm during execution.
+ * 
+ * @see Inplaces
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE_USE)
 public @interface Mutable {

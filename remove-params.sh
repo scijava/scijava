32d3c6e091693cdae103d985b320d86e05499ee6
Gabriel Selzer <gjselzer@wisc.edu>
2021-03-25 10:17:45 -0500

    Remove remove-params.sh script
    It has outlived its usefulness to us. RIP

--- remove-params.sh
+++
@@ -1,9 +0,0 @@
-#perl -0777 -i -pe 's/(\@plugin[^\n]*\n)\@Parameter.key = "([^"]*)".\n/\1\/**\n * TODO\n *\n * \@param \2\n/igs' **/*.java
-#perl -0777 -i -pe 's/\@Parameter.key = "([^"]*)"[^\n]*.\n/ * \@param \1\n/igs' **/*.java
-#perl -0777 -i -pe 's/( \* \@param[^\n]*\n)([^ ])/\1 *\/\n\2/igs' **/*.java
-#perl -0777 -i -pe 's/ \*\/\n([\s]*[\*])/\1/igs' **/*.java
-#perl -0777 -i -pe 's/ \*\/\n([\s]*\@Parameter)/\1/igs' **/*.java
-#perl -0777 -i -pe 's/ \*\/\n([\s]*\/\/[\s]*\*)/\1/igs' **/*.java
-#perl -0777 -i -pe 's/ \* \@param output([^\n]*\n[\s]*\*\/\n[\s]*public[^\(]*apply\()/ \* \@return the output\1/igs' **/*.java
-perl -0777 -i -pe 's/(\s\@Override[^\n]*)\n([\s]*\/\*\*[^\/]*\/)/\2\n\1/igs' **/*.java
-

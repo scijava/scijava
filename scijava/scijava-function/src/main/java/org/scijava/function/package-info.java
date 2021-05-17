c90f062452885c3694bee4276c6dfda0a081427f
Gabriel Selzer <gjselzer@wisc.edu>
2021-05-17 15:52:18 -0500

    Add package-info.java

---
+++ scijava/scijava-function/src/main/java/org/scijava/function/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * Provides a set of lightweight <b>functional interfaces</b>.
+ * <p>
+ * The package is named in accordance with {@code java.util.function}, as the
+ * Function and Consumer interfaces housed within this package are designed as
+ * extensions of {@link java.util.function.Function} /
+ * {@link java.util.function.BiFunction} and {@link java.util.function.Consumer}
+ * / {@link java.util.function.BiConsumer} respectively. The name should
+ * <b>not</b> be confused with the usage of "function" within SciJava Ops or any
+ * other project.
+ * <p>
+ * 
+ * @author Gabriel Selzer
+ */
+
+package org.scijava.function;

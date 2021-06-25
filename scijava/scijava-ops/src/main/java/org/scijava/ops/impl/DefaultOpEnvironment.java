NicoKiaru/incubator

Migrate ops references to Computers, annotations
Functions/Inplaces to come

scijava/scijava-ops/src/main/java/org/scijava/ops/impl/DefaultOpEnvironment.java
@@ -64,8 +64,8 @@ import org.scijava.ops.OpMethod;
 import org.scijava.ops.OpUtils;
 import org.scijava.ops.core.Op;
 import org.scijava.ops.core.OpCollection;
-import org.scijava.ops.function.Computers;
-import org.scijava.ops.function.Computers.Arity1;
+import org.scijava.functions.Computers;
+import org.scijava.functions.Computers.Arity1;
 import org.scijava.ops.matcher.DefaultOpMatcher;
 import org.scijava.ops.matcher.MatchingResult;
 import org.scijava.ops.matcher.MatchingUtils;

scijava/scijava

scijava/scijava-ops/src/main/java/org/scijava/ops/impl/DefaultOpEnvironment.java
@@ -61,9 +61,9 @@ import org.scijava.ops.OpMethod;
 import org.scijava.ops.OpUtils;
 import org.scijava.ops.core.Op;
 import org.scijava.ops.core.OpCollection;
-import org.scijava.ops.hints.Hints;
 import org.scijava.ops.hints.BaseOpHints.Adaptation;
 import org.scijava.ops.hints.BaseOpHints.Simplification;
+import org.scijava.ops.hints.Hints;
 import org.scijava.ops.hints.impl.AdaptationHints;
 import org.scijava.ops.hints.impl.DefaultHints;
 import org.scijava.ops.hints.impl.SimplificationHints;
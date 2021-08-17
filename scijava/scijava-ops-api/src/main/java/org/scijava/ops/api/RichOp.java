e3e0a18f4a67509f2a53d48b77e761548f845e4a
Gabriel Selzer <gjselzer@wisc.edu>
2021-08-17 16:25:33 -0500

    Add asOpType method

--- scijava/scijava-ops-api/src/main/java/org/scijava/ops/api/RichOp.java
+++ scijava/scijava-ops-api/src/main/java/org/scijava/ops/api/RichOp.java
@@ -9,6 +9,19 @@ public interface RichOp<T> extends GenericTyped {
 
 	T op();
 
+	/**
+	 * Returns this {@link RichOp} as its op interface {@link Type}
+	 * <p>
+	 * NB Implementations of {@link RichOp} must also implement the op interface
+	 * to ensure that this method works.
+	 * 
+	 * @return this {@link RichOp} as the type of its op interface
+	 */
+	@SuppressWarnings("unchecked")
+	default T asOpType() {
+		return (T) this;
+	}
+
 	OpMetadata metadata();
 
 	void preprocess(Object... inputs);

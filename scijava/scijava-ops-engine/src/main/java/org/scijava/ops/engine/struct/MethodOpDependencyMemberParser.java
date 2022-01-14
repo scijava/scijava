19428c8854c5d3349a10f59079eb4776b904e67e
Gabriel Selzer <gjselzer@wisc.edu>
2022-01-14 16:00:59 -0600

    Clean up MethodOpDependencyMemberParser

--- scijava/scijava-ops-engine/src/main/java/org/scijava/ops/engine/struct/MethodOpDependencyMemberParser.java
+++ scijava/scijava-ops-engine/src/main/java/org/scijava/ops/engine/struct/MethodOpDependencyMemberParser.java
@@ -13,57 +13,73 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-import org.scijava.ValidityProblem;
 import org.scijava.function.Producer;
 import org.scijava.ops.spi.OpDependency;
 import org.scijava.struct.MemberParser;
+import org.scijava.struct.Struct;
 import org.scijava.struct.ValidityException;
 
+/**
+ * Looks for {@link OpDependency} annotations on
+ * {@link java.lang.reflect.Parameter}s. For each such parameter with an
+ * {@link OpDependency} annotation, this class creates a
+ * {@link MethodParameterOpDependencyMember}.
+ *
+ * @author Gabriel Selzer
+ */
 public class MethodOpDependencyMemberParser implements
 	MemberParser<Method, MethodParameterOpDependencyMember<?>>
 {
 
 	private static final Map<Method, MethodJavadoc> methodDocMap = new ConcurrentHashMap<>();
 
+	/**
+	 * Parses out the {@link MethodParameterOpDependencyMember}s from {@code
+	 * source}
+	 * 
+	 * @param source the {@link Object} to parse
+	 * @param structType TODO
+	 * @return a {@link List} of all {@link MethodParameterOpDependencyMember}s
+	 *         described via the {@link OpDependency} annotation in {@code source}
+	 * @throws ValidityException
+	 */
 	@Override
-	public List<MethodParameterOpDependencyMember<?>> parse(Method source, Type structType)
-		throws ValidityException
+	public List<MethodParameterOpDependencyMember<?>> parse(Method source,
+		Type structType) throws ValidityException
 	{
 		if (source == null) return null;
 
 		source.setAccessible(true);
 
 		final ArrayList<MethodParameterOpDependencyMember<?>> items = new ArrayList<>();
-		final ArrayList<ValidityProblem> problems = new ArrayList<>();
 
 		// Parse method level @OpDependency annotations.
 		parseMethodOpDependencies(items, source);
-
-		// Fail if there were any problems.
-		if (!problems.isEmpty()) throw new ValidityException(problems);
 
 		return items;
 	}
 
 	private static MethodJavadoc getDocData(Method annotatedMethod) {
-		return methodDocMap.computeIfAbsent(annotatedMethod, m -> RuntimeJavadoc
-			.getJavadoc(m));
+		return methodDocMap.computeIfAbsent(annotatedMethod,
+			RuntimeJavadoc::getJavadoc);
 	}
 
-	private static void parseMethodOpDependencies(final List<MethodParameterOpDependencyMember<?>> items,
+	private static void parseMethodOpDependencies(
+		final List<MethodParameterOpDependencyMember<?>> items,
 		final Method annotatedMethod)
 	{
 		// If the Op method has no dependencies, return false without looping
 		// through parameters
 		boolean hasOpDependencies = Arrays.stream(annotatedMethod.getParameters()) //
-				.anyMatch(param -> param.isAnnotationPresent(OpDependency.class));
+			.anyMatch(param -> param.isAnnotationPresent(OpDependency.class));
 		if (!hasOpDependencies) return;
 
 		final java.lang.reflect.Parameter[] methodParams = annotatedMethod
 			.getParameters();
 
 		for (int i = 0; i < methodParams.length; i++) {
-			final OpDependency dependency = methodParams[i].getAnnotation(OpDependency.class);
+			final OpDependency dependency = methodParams[i].getAnnotation(
+				OpDependency.class);
 			if (dependency == null) continue;
 
 			final int j = i;
@@ -76,12 +92,13 @@ public class MethodOpDependencyMemberParser implements
 			Producer<String> descriptionGenerator = () -> {
 				List<ParamJavadoc> params = getDocData(annotatedMethod).getParams();
 				if (params.size() <= j) return "";
-				return getDocData(annotatedMethod).getParams().get(j).getComment().toString();
+				return getDocData(annotatedMethod).getParams().get(j).getComment()
+					.toString();
 			};
 			final Type methodParamType = methodParams[i].getParameterizedType();
 			final MethodParameterOpDependencyMember<?> item =
-				new MethodParameterOpDependencyMember<>(nameGenerator, descriptionGenerator,
-					methodParamType, dependency);
+				new MethodParameterOpDependencyMember<>(nameGenerator,
+					descriptionGenerator, methodParamType, dependency);
 			items.add(item);
 		}
 	}

92cbe04f217f4e0023a159433ff0345f5ecebc20
hinerm <hinerm@gmail.com>
2022-11-17 13:28:19 -0600

    Factor out AbstractInfoReducer
    Eliminates the copy-and-paste between Function/Computer reducer and
    ensures consistency between both.

--- scijava/scijava-ops-engine/src/main/java/org/scijava/ops/engine/reduce/ComputerReducer.java
+++ scijava/scijava-ops-engine/src/main/java/org/scijava/ops/engine/reduce/ComputerReducer.java
@@ -1,34 +1,21 @@
 package org.scijava.ops.engine.reduce;
 
 import org.scijava.function.Computers;
-import org.scijava.ops.api.OpInfo;
-import org.scijava.ops.engine.OpUtils;
-import org.scijava.types.Types;
 
-import java.lang.reflect.Type;
-
-public class ComputerReducer implements InfoReducer{
+public class ComputerReducer extends AbstractInfoReducer {
 
 	@Override
-	public boolean canReduce(OpInfo info) {
-		return Computers.isComputer(OpUtils.findFunctionalInterface(Types.raw(info.opType())));
+	protected boolean isReducerType(Class<?> functionalInterface) {
+		return Computers.isComputer(functionalInterface);
 	}
 
 	@Override
-	public ReducedOpInfo reduce(OpInfo info, int numReductions) {
-		Type opType = info.opType();
-		Class<?> rawType = OpUtils.findFunctionalInterface(Types.raw(opType));
-		int originalArity = Computers.arityOf(rawType);
-		int reducedArity = originalArity - numReductions;
-		Class<?> reducedRawType = Computers.computerOfArity(reducedArity);
-		Type[] inputTypes = OpUtils.inputTypes(info.struct());
-		Type outputType = info.output().getType();
-		Type[] newTypes = new Type[reducedArity + 1];
-		if (reducedArity >= 0)
-			System.arraycopy(inputTypes, 0, newTypes, 0, reducedArity);
-		newTypes[newTypes.length - 1] = outputType;
-		Type reducedOpType = Types.parameterize(reducedRawType, newTypes);
-		return new ReducedOpInfo(info, reducedOpType, originalArity - reducedArity);
+	protected int arityOf(Class<?> rawType) {
+		return Computers.arityOf(rawType);
 	}
 
+	@Override
+	protected Class<?> ofArity(int reducedArity) {
+		return Computers.computerOfArity(reducedArity);
+	}
 }
--- scijava/scijava-ops-engine/src/main/java/org/scijava/ops/engine/reduce/FunctionReducer.java
+++ scijava/scijava-ops-engine/src/main/java/org/scijava/ops/engine/reduce/FunctionReducer.java
@@ -1,38 +1,22 @@
 package org.scijava.ops.engine.reduce;
 
-import java.lang.reflect.Type;
-
 import org.scijava.function.Functions;
-import org.scijava.ops.api.OpInfo;
-import org.scijava.ops.engine.OpUtils;
-import org.scijava.ops.api.features.BaseOpHints;
-import org.scijava.types.Types;
 
-public class FunctionReducer implements InfoReducer{
+public class FunctionReducer extends AbstractInfoReducer {
 
 	@Override
-	public boolean canReduce(OpInfo info) {
-		boolean isFunction = Functions.isFunction(OpUtils.findFunctionalInterface(Types.raw(info.opType())));
-		boolean canReduce = info.declaredHints().containsNone(
-				BaseOpHints.Reduction.FORBIDDEN);
-		return isFunction && canReduce;
+	protected boolean isReducerType(Class<?> functionalInterface) {
+		return Functions.isFunction(functionalInterface);
 	}
 
 	@Override
-	public ReducedOpInfo reduce(OpInfo info, int numReductions) {
-		Type opType = info.opType();
-		Class<?> rawType = OpUtils.findFunctionalInterface(Types.raw(opType));
-		int originalArity = Functions.arityOf(Functions.superType(rawType));
-		int reducedArity = originalArity - numReductions;
-		Class<?> reducedRawType = Functions.functionOfArity(reducedArity);
-		Type[] inputTypes = OpUtils.inputTypes(info.struct());
-		Type outputType = info.output().getType();
-		Type[] newTypes = new Type[reducedArity + 1];
-		if (reducedArity >= 0)
-			System.arraycopy(inputTypes, 0, newTypes, 0, reducedArity);
-		newTypes[newTypes.length - 1] = outputType;
-		Type reducedOpType = Types.parameterize(reducedRawType, newTypes);
-		return new ReducedOpInfo(info, reducedOpType, originalArity - reducedArity);
+	protected int arityOf(Class<?> rawType) {
+		return Functions.arityOf(Functions.superType(rawType));
+	}
+
+	@Override
+	protected Class<?> ofArity(int reducedArity) {
+		return Functions.functionOfArity(reducedArity);
 	}
 
 }

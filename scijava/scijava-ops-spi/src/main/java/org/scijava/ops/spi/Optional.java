a671c5ca991fa2991f1111f7c6728a646b402712
hinerm <hinerm@gmail.com>
2022-11-21 14:41:27 -0600

    Document lack of @Optional on lambdas

--- scijava/scijava-ops-spi/src/main/java/org/scijava/ops/spi/Optional.java
+++ scijava/scijava-ops-spi/src/main/java/org/scijava/ops/spi/Optional.java
@@ -36,6 +36,21 @@ import java.lang.annotation.Target;
  * it obvious that {@code in3} is optional just by looking at {@code Impl}? For
  * this reason, it should be enforced that the annotation is only on one of the
  * method signatures.
+ * <br/><br/>
+ * Note also that annotations are currently (as of Java 11) not supported on lambdas.
+ * Optional parameters are supported on interfaces and class implementations (including
+ * anonymous classes) but the following will not recognize the optionality of {@code in}:
+ * <pre>
+ * &#64;OpField(names = "optionalLambda")
+ * public final Function<Integer, Float> optionalLambda = (&#64;Optional Integer in) -> {
+ * 	Integer nonNullIn = in == null ? 0 : in;
+ * 	return nonNullIn + 0.5f;
+ * };
+ * </pre>
+ *
+ * See also these SO posts:
+ * <a href="https://stackoverflow.com/questions/44646915/will-it-be-possible-to-annotate-lambda-expression-in-java-9">one</a>,
+ * <a href="https://stackoverflow.com/questions/22375891/annotating-the-functional-interface-of-a-lambda-expression">two</a>
  *
  * @author Gabriel Selzer
  */

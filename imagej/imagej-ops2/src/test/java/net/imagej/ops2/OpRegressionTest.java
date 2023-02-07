0cd47b2fbd80de70804fd12e6021ffc7327a4b40
Mark Hiner <hinerm@gmail.com>
2023-02-07 17:34:19 +0100

    RichardsonLucyTVF: add Optional params

--- imagej/imagej-ops2/src/test/java/net/imagej/ops2/OpRegressionTest.java
+++ imagej/imagej-ops2/src/test/java/net/imagej/ops2/OpRegressionTest.java
@@ -39,7 +39,7 @@ public class OpRegressionTest extends AbstractOpTest {
 
 	@Test
 	public void opDiscoveryRegressionIT() {
-		long expected = 1380;
+		long expected = 1381;
 		long actual = StreamSupport.stream(ops.infos().spliterator(), false).count();
 		assertEquals(expected, actual);
 	}

464151f2d5ef733c42cb494d0c812d6d56be7dad
hinerm <hinerm@gmail.com>
2023-04-11 14:29:38 -0500

    DefaultOpEnv: fix javadoc typo

--- scijava/scijava-ops-engine/src/main/java/org/scijava/ops/engine/DefaultOpEnvironment.java
+++ scijava/scijava-ops-engine/src/main/java/org/scijava/ops/engine/DefaultOpEnvironment.java
@@ -410,7 +410,7 @@ public class DefaultOpEnvironment implements OpEnvironment {
 	 * Finds an Op instance matching the request described by {@link OpRef}
 	 * {@code ref} and stores this Op in {@link #opCache}. NB the return must be an
 	 * {@link Object} here (instead of some type variable T where T is the Op
-	 * type} since there is no way to ensure that the {@code OpRef} can provide
+	 * type) since there is no way to ensure that the {@code OpRef} can provide
 	 * that T (since the OpRef could require that the Op returned is of multiple
 	 * types).
 	 * 

e7f7100833df07b5c6ef11438a8db3614bd68881
Gabriel Selzer <gjselzer@wisc.edu>
2023-06-20 10:07:09 -0500

    Test validity of OpMethod dependency positions

--- scijava/scijava-ops-engine/src/test/java/org/scijava/ops/engine/OpMethodDependencyPositionTest.java
+++ scijava/scijava-ops-engine/src/test/java/org/scijava/ops/engine/OpMethodDependencyPositionTest.java
@@ -9,7 +9,10 @@ import java.util.function.Function;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.scijava.common3.validity.ValidityException;
 import org.scijava.function.Computers;
+import org.scijava.ops.engine.hint.DefaultHints;
+import org.scijava.ops.engine.matcher.impl.OpMethodInfo;
 import org.scijava.ops.spi.OpCollection;
 import org.scijava.ops.spi.OpDependency;
 import org.scijava.ops.spi.OpField;
@@ -18,20 +21,8 @@ import org.scijava.ops.spi.OpMethod;
 public class OpMethodDependencyPositionTest extends AbstractTestEnvironment
 		implements OpCollection {
 
-	@BeforeAll
-	public static void addNeededOps() {
-		ops.register(new OpMethodDependencyPositionTest());
-	}
-
-	@OpField(names = "test.stringToLong")
-	public final Function<String, Long> parser = in -> Long.parseLong(in);
-
-	@OpMethod( //
-		names = "test.dependencyBeforeOutput", //
-		type = Computers.Arity1.class //
-	)
-	public static void OpMethodFoo( //
-		@OpDependency(name = "test.stringToLong") Function<String, Long> op, //
+	public static void goodDep( //
+		@OpDependency(name = "someDep") Function<String, Long> op, //
 		List<String> in, //
 		List<Long> out //
 	) {
@@ -41,13 +32,82 @@ public class OpMethodDependencyPositionTest extends AbstractTestEnvironment
 	}
 
 	@Test
-	public void testOpDependencyBeforeOutput() {
-		List<String> in = new ArrayList<>();
-		in.add("1");
-		List<Long> out = new ArrayList<>();
-		ops.op("test.dependencyBeforeOutput").arity1().input(in).output(out).compute();
-		List<Long> expected = Arrays.asList(1l);
-		assertIterationsEqual(expected, out);
+	public void testOpDependencyBefore() throws NoSuchMethodException {
+		var m = this.getClass().getDeclaredMethod(//
+			"goodDep", //
+			Function.class, //
+			List.class, //
+			List.class //
+		);
+		var info = new OpMethodInfo( //
+			m, //
+			Computers.Arity1.class, //
+			new DefaultHints(), //
+			"test.dependencyBeforeInput" //
+		);
+		Assertions.assertEquals(null, info.getValidityException());
+	}
+
+	public static void badDep( //
+			List<String> in, //
+			@OpDependency(name = "someDep") Function<String, Long> op, //
+			List<Long> out //
+	) {
+		out.clear();
+		for (String s : in)
+			out.add(op.apply(s));
+	}
+
+	@Test
+	public void testOpDependencyAfter() throws NoSuchMethodException {
+		var m = this.getClass().getDeclaredMethod(//
+				"badDep", //
+				List.class, //
+				Function.class, //
+				List.class //
+		);
+		var info = new OpMethodInfo( //
+					m, //
+					Computers.Arity1.class, //
+					new DefaultHints(), //
+					"test.dependencyAfterInput" //
+		);
+		ValidityException exc = info.getValidityException();
+		String expMsg = "java.lang.IllegalArgumentException: Op Dependencies in " +
+			"static methods must come before any other parameters!";
+		Assertions.assertEquals(expMsg, exc.problems().get(0).getMessage());
+	}
+
+	public static void goodThenBadDep( //
+			@OpDependency(name = "someDep") Function<String, Long> op, //
+			List<String> in, //
+			@OpDependency(name = "someOtherDep") Function<String, Long> op2, //
+			List<Long> out //
+	) {
+		out.clear();
+		for (String s : in)
+			out.add(op.apply(s));
+	}
+
+	@Test
+	public void testOpDependencyBeforeAndAfter() throws NoSuchMethodException {
+		var m = this.getClass().getDeclaredMethod(//
+				"goodThenBadDep", //
+				Function.class, //
+				List.class, //
+				Function.class, //
+				List.class //
+		);
+		var info = new OpMethodInfo( //
+				m, //
+				Computers.Arity1.class, //
+				new DefaultHints(), //
+				"test.dependencyBeforeAndAfterInput" //
+		);
+		ValidityException exc = info.getValidityException();
+		String expMsg = "java.lang.IllegalArgumentException: Op Dependencies in " +
+				"static methods must come before any other parameters!";
+		Assertions.assertEquals(expMsg, exc.problems().get(0).getMessage());
 	}
 
 }

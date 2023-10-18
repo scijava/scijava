5bd5a4334870813f8ee80baadf603d0dcd456b31
Gabriel Selzer <gjselzer@wisc.edu>
2023-10-18 13:33:58 -0500

    Test InfoTreeGenerator priority

---
+++ scijava/scijava-ops-engine/src/test/java/org/scijava/ops/engine/InfoTreeGeneratorTest.java
@@ -0,0 +1,60 @@
+package org.scijava.ops.engine;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.function.Function;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.scijava.ops.api.InfoTree;
+import org.scijava.ops.api.OpInfo;
+import org.scijava.ops.engine.matcher.impl.OpFieldInfo;
+import org.scijava.ops.spi.OpCollection;
+import org.scijava.ops.spi.OpField;
+import org.scijava.priority.Priority;
+
+public class InfoTreeGeneratorTest extends AbstractTestEnvironment implements
+		OpCollection
+{
+
+	@OpField(names="test.infoTreeGeneration")
+	public final Function<Double, Double> foo = in -> in + 1;
+
+	@BeforeAll
+	public static void addNeededOps() {
+		ops.register(new DummyInfoTreeGenerator(), new InfoTreeGeneratorTest());
+	}
+
+
+	@Test
+	public void testMultipleValidInfoTreeGenerators() {
+		// Obtain a signature that can be reified by the DefaultInfoTreeGenerator and by our dummy generator
+		var op = ops.unary("test.infoTreeGeneration").inType(Double.class).outType(Double.class).function();
+		String signature = ops.history().signatureOf(op);
+		// Run treeFromID to make sure our generator doesn't run
+		var infoTree = ops.treeFromID(signature);
+		// Assert non-null output
+		Assertions.assertNotNull(infoTree);
+	}
+
+
+	static class DummyInfoTreeGenerator implements InfoTreeGenerator {
+
+		@Override
+		public InfoTree generate(String signature, Map<String, OpInfo> idMap,
+				Collection<InfoTreeGenerator> generators)
+		{
+			return null;
+		}
+
+		@Override public boolean canGenerate(String signature) {
+			return true;
+		}
+
+		@Override public double getPriority() {
+			return Priority.LOW;
+		}
+
+	}
+}

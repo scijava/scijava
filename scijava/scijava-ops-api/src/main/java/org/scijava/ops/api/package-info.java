282b3ef51e697e811f40880df8c5a8a2e0b2dfe3
Gabriel Selzer <gjselzer@wisc.edu>
2023-10-25 13:21:53 -0500

    Op Equivalence -> Algorithmic Equivalence

--- scijava/scijava-ops-api/src/main/java/org/scijava/ops/api/package-info.java
+++ scijava/scijava-ops-api/src/main/java/org/scijava/ops/api/package-info.java
@@ -14,7 +14,7 @@
  * To support the Op matching paradigm, we establish three types of equivalence:
  * <ol>
  *   <li><b>Form Equivalence</b></li> Drawn from <a href="https://en.wikipedia.org/wiki/Theory_of_forms">Plato's Theory of Forms</a>, Form Equivalence implies that two objects (which could be Ops, or Op parameters) theoretically draw from the same shared idea (such as an addition operation, or an image, etc.)
- *   <li><b>Op Equivalence</b></li> Algorithmic Equivalence means that two Ops:
+ *   <li><b>Algorithmic Equivalence</b></li> Algorithmic Equivalence means that two Ops:
  *   <ol>
  *     <li>Are Form Equivalent</li>
  *     <li>Have the same number of inputs and the same number of outputs</li>
@@ -25,8 +25,8 @@
  * </ol>
  * Within the Ops API, each type of equivalence is utilized in the following ways:
  * <ol>
- *   <li><b>Form Equivalence</b></li> If two Ops are "Form Equivalent", they are defined under the same name.
- *   <li><b>Op Equivalence</b></li> If two Ops are "Op Equivalent", then they are considered to implement the same computational algorithm.
+ *   <li><b>Form Equivalence</b></li> If two Ops are "Form Equivalent", they are defined under the same name, i.e. {@code op1.name().equals(op2.name())}
+ *   <li><b>Algorithmic Equivalence</b></li> If two Ops are "Algorithm Equivalent", then they are considered to implement the same computational algorithm.
  *   <li><b>Numerical Equivalence</b></li> If two Ops are "Numerically Equivalent", then they are considered to output the same values, neglecting trivial differences such as floating-point errors.
  * </ol>
  *
@@ -36,12 +36,11 @@
  *   <li> filter.gauss(ij.ImagePlus, java.lang.Double) -> ij.ImagePlus</li>
  *   <li> filter.gauss(net.imglib2.img.Img, net.imglib2.algorithm.neighborhood.Shape) -> net.imglib2.img.Img</li>
  * </ol>
- * Ops 1 and 2 should be considered form equivalent, as they have the same name, and Op equivalent, as they both take in an image data structure and a floating point number and return an image data structure, but they are likely not numerically equivalent due to the underlying data structures.
+ * Ops 1 and 2 should be considered form equivalent, as they have the same name, and algorithmically equivalent, as they both take in an image data structure and a floating point number and return an image data structure, but they are likely not numerically equivalent due to the underlying data structures.
  * <p>
  * Ops 1 and 3 should also be considered form equivalent, as they have the same name, but are not form equivalent, as one computes its own Shape over which to perform a gaussian blur, while the other takes a predefined shape.
  * <p>
  * These definitions of equivalence provide a level of flexibility impossible without the Ops API; form equivalence allows us to, for example, define equivalent Ops across programming languages and libraries, and then to create scripts that can run unaltered on data types from each of those languages and libraries. Within each form implementation, however, we ensure numeric equivalence and therefore reproducability
- *
  *
  */
 package org.scijava.ops.api;

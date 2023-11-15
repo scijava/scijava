80b32fd286f053ee44a781593e1d817f15b460bb
hinerm <hinerm@gmail.com>
2023-11-15 14:57:07 -0600

    Add more BinaryRealType tests
    All the implemented BinaryRealType ops should now be covered, with all
    aliases.

--- imagej/imagej-ops2/src/test/java/net/imagej/ops2/math/BinaryRealTypeMathTest.java
+++ imagej/imagej-ops2/src/test/java/net/imagej/ops2/math/BinaryRealTypeMathTest.java
@@ -33,6 +33,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import net.imagej.ops2.AbstractOpTest;
 import net.imglib2.type.numeric.integer.IntType;
+import net.imglib2.util.Util;
 import org.junit.jupiter.api.Test;
 import org.scijava.ops.engine.math.MathOps;
 
@@ -43,8 +44,10 @@ import org.scijava.ops.engine.math.MathOps;
  */
 public class BinaryRealTypeMathTest extends AbstractOpTest {
 
-	private static final IntType A = new IntType(25);
-	private static final IntType B = new IntType(25);
+	private static final IntType A = new IntType(12);
+	private static final IntType B = new IntType(7);
+
+	// ADD
 	@Test
 	public void testAddF() {
 		for (String opName : MathOps.ADD.split(", ")) {
@@ -61,6 +64,8 @@ public class BinaryRealTypeMathTest extends AbstractOpTest {
 			assertEquals(A.get() + B.get(), c.get());
 		}
 	}
+
+	// SUB
 
 	@Test
 	public void testSubF() {
@@ -79,11 +84,13 @@ public class BinaryRealTypeMathTest extends AbstractOpTest {
 		}
 	}
 
+	// DIV
+
 	@Test
 	public void testDivF() {
 		for (String opName : MathOps.DIV.split(", ")) {
 			IntType c = ops.binary(opName).input(A, B).outType(IntType.class).apply();
-			assertEquals(A.get() / B.get(), c.get());
+			assertEquals(Util.round(A.getRealDouble() / B.get()), c.get());
 		}
 	}
 
@@ -92,9 +99,11 @@ public class BinaryRealTypeMathTest extends AbstractOpTest {
 		for (String opName : MathOps.DIV.split(", ")) {
 			IntType c = new IntType();
 			ops.binary(opName).input(A, B).output(c).compute();
-			assertEquals(A.get() / B.get(), c.get());
+			assertEquals(Util.round(A.getRealDouble() / B.get()), c.get());
 		}
 	}
+
+	// MUL
 
 	@Test
 	public void testMulF() {
@@ -111,6 +120,89 @@ public class BinaryRealTypeMathTest extends AbstractOpTest {
 			ops.binary(opName).input(A, B).output(c).compute();
 			assertEquals(A.get() * B.get(), c.get());
 		}
+	}
+
+	// POW
+
+	@Test
+	public void testPowF() {
+		for (String opName : MathOps.POW.split(", ")) {
+			IntType c = ops.binary(opName).input(A, B).outType(IntType.class).apply();
+			assertEquals(Math.pow(A.get(), B.get()), c.get());
+		}
+	}
+
+	@Test
+	public void testPowC() {
+		for (String opName : MathOps.POW.split(", ")) {
+			IntType c = new IntType();
+			ops.binary(opName).input(A, B).output(c).compute();
+			assertEquals(Math.pow(A.get(), B.get()), c.get());
+		}
+	}
+
+	// MOD
+
+	@Test
+	public void testModF() {
+		for (String opName : MathOps.MOD.split(", ")) {
+			IntType c = ops.binary(opName).input(A, B).outType(IntType.class).apply();
+			assertEquals(A.get() % B.get(), c.get());
+		}
+	}
+
+	@Test
+	public void testModC() {
+		for (String opName : MathOps.MOD.split(", ")) {
+			IntType c = new IntType();
+			ops.binary(opName).input(A, B).output(c).compute();
+			assertEquals(A.get() % B.get(), c.get());
+		}
+	}
+
+	// OR
+
+	@Test
+	public void testOrF() {
+		IntType c = ops.binary("math.or").input(A, B).outType(IntType.class).apply();
+		assertEquals((long)A.get() | (long)B.get(), c.get());
+	}
+
+	@Test
+	public void testOrC() {
+		IntType c = new IntType();
+		ops.binary("math.or").input(A, B).output(c).compute();
+		assertEquals((long)A.get() | (long)B.get(), c.get());
+	}
+
+	// XOR
+
+	@Test
+	public void testXorF() {
+		IntType c = ops.binary("math.xor").input(A, B).outType(IntType.class).apply();
+		assertEquals((long)A.get() ^ (long)B.get(), c.get());
+	}
+
+	@Test
+	public void testXorC() {
+		IntType c = new IntType();
+		ops.binary("math.xor").input(A, B).output(c).compute();
+		assertEquals((long)A.get() ^ (long)B.get(), c.get());
+	}
+
+	// AND
+
+	@Test
+	public void testAndF() {
+		IntType c = ops.binary("math.and").input(A, B).outType(IntType.class).apply();
+		assertEquals((long)A.get() & (long)B.get(), c.get());
+	}
+
+	@Test
+	public void testAndC() {
+		IntType c = new IntType();
+		ops.binary("math.and").input(A, B).output(c).compute();
+		assertEquals((long)A.get() & (long)B.get(), c.get());
 	}
 
 // FIXME: inplaces would be nice

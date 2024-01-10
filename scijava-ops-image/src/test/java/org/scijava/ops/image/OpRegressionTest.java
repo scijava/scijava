0ba7f92e656440f034d4df468e9ace09d752f50c
Gabriel Selzer <gjselzer@wisc.edu>
2024-01-10 16:00:14 -0600

    Beautify FFT Ops

--- scijava-ops-image/src/test/java/org/scijava/ops/image/OpRegressionTest.java
+++ scijava-ops-image/src/test/java/org/scijava/ops/image/OpRegressionTest.java
@@ -42,7 +42,7 @@ public class OpRegressionTest {
 
 	@Test
 	public void opDiscoveryRegressionIT() {
-		long expected = 1875;
+		long expected = 1877;
 		long actual = ops.infos().size();
 		assertEquals(expected, actual);
 	}

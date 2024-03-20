4f1cb4bfd9a6dc3a2e50f17f11b186ec54837ef6
Gabriel Selzer <gjselzer@wisc.edu>
2024-03-20 08:57:29 -0500

    OpenCVDescriptors: Add Javadoc

--- scijava-ops-opencv/src/main/java/org/scijava/ops/opencv/OpenCVDescriptors.java
+++ scijava-ops-opencv/src/main/java/org/scijava/ops/opencv/OpenCVDescriptors.java
@@ -6,6 +6,11 @@ import org.bytedeco.opencv.opencv_core.Mat;
 import org.bytedeco.opencv.opencv_core.UMat;
 import org.scijava.types.Nil;
 
+/**
+ * A set of static methods intended to describe OpenCV data structures.
+ *
+ * @author Gabriel Selzer
+ */
 public class OpenCVDescriptors {
 
 	public static String matDescriptor(Nil<Mat> in) {

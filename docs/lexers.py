bf3e39e98dbea746a9020b5c6da67e80d3424336
Edward Evans <elevans2@wisc.edu>
2024-03-27 11:23:52 -0500

    Subclass the standard Groovy lexer
    Instead of duplicating the Groovy lexer to
    modify it, we can simply subclass it
    and inherit it's tokens.
    
    This commit also renames the lexer to
    "SciJavaGroovyLexer".

--- docs/lexers.py
+++ docs/lexers.py
@@ -1,82 +1,11 @@
-import re
-from pygments.lexer import RegexLexer, bygroups, using, this, default
-from pygments.token import Comment, Operator, Keyword, Name, String, Number, Whitespace
-from pygments.util import shebang_matches
+from pygments.lexer import inherit
+from pygments.lexers.jvm import GroovyLexer
+from pygments.token import Comment
 
-class FijiGroovyLexer(RegexLexer):
-    """
-    For Groovy source code.
-
-    .. versionadded:: 1.5
-    """
-
-    name = 'Groovy'
-    url = 'https://groovy-lang.org/'
-    aliases = ['fijigroovy']
-    filenames = ['*.groovy','*.gradle']
-    mimetypes = ['text/x-groovy']
-
-    flags = re.MULTILINE | re.DOTALL
-
+class SciJavaGroovyLexer(GroovyLexer):
     tokens = {
-        'root': [
-            # Groovy allows a file to start with a shebang
-            (r'#!(.*?)$', Comment.Preproc, 'base'),
-            default('base'),
-        ],
         'base': [
-            (r'[^\S\n]+', Whitespace),
             (r'#@.*?$', Comment.Single),
-            (r'(//.*?)(\n)', bygroups(Comment.Single, Whitespace)),
-            (r'/\*.*?\*/', Comment.Multiline),
-            # keywords: go before method names to avoid lexing "throw new XYZ"
-            # as a method signature
-            (r'(assert|break|case|catch|continue|default|do|else|finally|for|'
-             r'if|goto|instanceof|new|return|switch|this|throw|try|while|in|as)\b',
-             Keyword),
-            # method names
-            (r'^(\s*(?:[a-zA-Z_][\w.\[\]]*\s+)+?)'  # return arguments
-             r'('
-             r'[a-zA-Z_]\w*'                        # method name
-             r'|"(?:\\\\|\\[^\\]|[^"\\])*"'         # or double-quoted method name
-             r"|'(?:\\\\|\\[^\\]|[^'\\])*'"         # or single-quoted method name
-             r')'
-             r'(\s*)(\()',                          # signature start
-             bygroups(using(this), Name.Function, Whitespace, Operator)),
-            (r'@[a-zA-Z_][\w.]*', Name.Decorator),
-            (r'(abstract|const|enum|extends|final|implements|native|private|'
-             r'protected|public|static|strictfp|super|synchronized|throws|'
-             r'transient|volatile)\b', Keyword.Declaration),
-            (r'(def|boolean|byte|char|double|float|int|long|short|void)\b',
-             Keyword.Type),
-            (r'(package)(\s+)', bygroups(Keyword.Namespace, Whitespace)),
-            (r'(true|false|null)\b', Keyword.Constant),
-            (r'(class|interface)(\s+)', bygroups(Keyword.Declaration, Whitespace),
-             'class'),
-            (r'(import)(\s+)', bygroups(Keyword.Namespace, Whitespace), 'import'),
-            (r'""".*?"""', String.Double),
-            (r"'''.*?'''", String.Single),
-            (r'"(\\\\|\\[^\\]|[^"\\])*"', String.Double),
-            (r"'(\\\\|\\[^\\]|[^'\\])*'", String.Single),
-            (r'\$/((?!/\$).)*/\$', String),
-            (r'/(\\\\|\\[^\\]|[^/\\])*/', String),
-            (r"'\\.'|'[^\\]'|'\\u[0-9a-fA-F]{4}'", String.Char),
-            (r'(\.)([a-zA-Z_]\w*)', bygroups(Operator, Name.Attribute)),
-            (r'[a-zA-Z_]\w*:', Name.Label),
-            (r'[a-zA-Z_$]\w*', Name),
-            (r'[~^*!%&\[\](){}<>|+=:;,./?-]', Operator),
-            (r'[0-9][0-9]*\.[0-9]+([eE][0-9]+)?[fd]?', Number.Float),
-            (r'0x[0-9a-fA-F]+', Number.Hex),
-            (r'[0-9]+L?', Number.Integer),
-            (r'\n', Whitespace)
-        ],
-        'class': [
-            (r'[a-zA-Z_]\w*', Name.Class, '#pop')
-        ],
-        'import': [
-            (r'[\w.]+\*?', Name.Namespace, '#pop')
-        ],
+            inherit,
+        ]
     }
-
-    def analyse_text(text):
-        return shebang_matches(text, r'groovy')

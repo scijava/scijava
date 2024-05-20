2e4222c86a4d528294f9d29bc6265d755101a638
Gabriel Selzer <gjselzer@wisc.edu>
2024-05-20 09:25:05 -0500

    Ensure taglets and indexer built first

--- .github/build.sh
+++ .github/build.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/main/ci-build.sh
-
-# NB: These two components must be pre-built before the project as a whole,
-# because they are used as helpers during the project build itself.
-mvn -Djavadoc.skip -pl scijava-taglets,scijava-ops-indexer -am clean install
-
 sh ci-build.sh

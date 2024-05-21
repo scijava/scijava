520610f8f157f3228a71dcd174faa1cc6ee1b372
Curtis Rueden <ctrueden@wisc.edu>
2024-05-20 22:42:37 -0500

    Fix scijava-ops-api package-info javadoc errors

--- scijava-ops-api/src/main/java/org/scijava/ops/api/package-info.java
+++ scijava-ops-api/src/main/java/org/scijava/ops/api/package-info.java
@@ -29,53 +29,61 @@
 /**
  * This module contains the API necessary to retrieve, execute, and reason about
  * Ops.
- * <h2>What is the SciJava Ops Library?</h2> SciJava Ops arose from a desire for
- * simplicity - there are lots of different algorithmic libraries, each with
- * their own data structures, algorithm syntax, and other requirements. With all
- * of these differences, library interoperability can be a daunting task. This
- * can lead to algorithm reimplementations, unmaintained code, and a lack of
- * cooperation between open-source communities. SciJava Ops seeks to unify
- * different libraries under a single framework, separating the development of
- * new algorithms from the way they are executed. Put more succinctly, the goals
- * of SciJava Ops are the following:
+ * <h2>What is the SciJava Ops Library?</h2>
+ * <p>
+ * SciJava Ops arose from a desire for simplicity - there are lots of different
+ * algorithmic libraries, each with their own data structures, algorithm syntax,
+ * and other requirements. With all of these differences, library
+ * interoperability can be a daunting task. This can lead to algorithm
+ * reimplementations, unmaintained code, and a lack of cooperation between
+ * open-source communities. SciJava Ops seeks to unify different libraries under
+ * a single framework, separating the development of new algorithms from the way
+ * they are executed. Put more succinctly, the goals of SciJava Ops are the
+ * following:
+ * </p>
  * <ul>
- * <li><b>Usability</b> All Ops should be called in the same way, and
+ * <li><b>Usability.</b> All Ops should be called in the same way, and
  * implementation details should be hidden - this means that the user should
  * call an Op the same way, whether the algorithm is written in Java, Python, or
  * as a CUDA kernel.</li>
- * <li><b>Extensibility</b> New Ops should be accommodated in a variety of
+ * <li><b>Extensibility.</b> New Ops should be accommodated in a variety of
  * programming languages, and each language should provide minimally-invasive
  * methods for declaring code blocks as Ops. This also means that Ops should
  * accommodate writing Ops in one language, and then calling Ops using data
  * structures from a different language.</li>
- * <li><b>Generality</b> Ops should allow any number of typed inputs, without
+ * <li><b>Generality.</b> Ops should allow any number of typed inputs, without
  * restriction on input types. With no restrictions on input types, the library
  * can seamlessly adapt new data structures tailored to performance or
  * expressiveness as they are developed.</li>
- * <li><b>Speed</b> Calling Ops should be approximately as performant as
+ * <li><b>Speed.</b> Calling Ops should be approximately as performant as
  * invocation in their natural setting (such as Python, C++, Java, or another
  * language entirely).</li>
  * </ul>
- * <h2>What is an Op?</h2> An Op is an algorithm adhering to the following
- * traits:
+ * <h2>What is an Op?</h2>
+ * <p>
+ * An Op is an algorithm adhering to the following traits:
+ * </p>
  * <ol>
  * <li>Ops are stateless and deterministic - with no internal state, calling an
  * Op two times on the same inputs will produce the same output.</li>
  * <li>Ops are named - this name conveys an Op's purpose, and allows us to find
  * all Ops implementing a particular operation</li>
  * </ol>
+ * <p>
  * Using the name and the combination of input and output parameters, we can
- * retrieve, or "match", any Op from within an
- * {@link org.scijava.ops.api.OpEnvironment}. Op calls with the same name and
- * specified inputs/outputs will be reproducible within a particular Op
- * environment.
- * <h2>Op Equivalence</h2> To support the Op matching paradigm, we establish
- * three types of equivalence:
+ * retrieve, or "match", any Op from within an {@link OpEnvironment}. Op calls
+ * with the same name and specified inputs/outputs will be reproducible within a
+ * particular Op environment.
+ * </p>
+ * <h2>Op Equivalence</h2>
+ * <p>
+ * To support the Op matching paradigm, we establish three types of equivalence:
+ * </p>
  * <ol>
- * <li><b>Form Equivalence</b></li> Form Equivalence implies that two objects
- * (which could be Ops, or Op parameters) theoretically draw from the same
- * shared idea (such as an addition operation, or an image, etc.)
- * <li><b>Structural Equivalence</b></li> Structural Equivalence means that two
+ * <li><b>Form Equivalence</b> implies that two objects (which could be Ops, or
+ * Op parameters) theoretically draw from the same shared idea (such as an
+ * addition operation, or an image, etc.)</li>
+ * <li><b>Structural Equivalence</b> - Structural Equivalence means that two
  * Ops:
  * <ol>
  * <li>Are Form Equivalent</li>
@@ -83,45 +91,53 @@
  * <li>For each input position, accept form-equivalent inputs</li>
  * <li>Return form-equivalent outputs</li>
  * </ol>
- * <li><b>Result Equivalence</b></li> Result Equivalence means that
- * {@code o1.equals(o2)} for two outputs {@code o1} and {@code o2} from two Ops.
+ * </li>
+ * <li><b>Result Equivalence</b> means that {@code o1.equals(o2)} for two
+ * outputs {@code o1} and {@code o2} from two Ops.</li>
  * </ol>
+ * <p>
  * Within the Ops API, each type of equivalence is utilized in the following
  * ways:
+ * </p>
  * <ol>
- * <li><b>Form Equivalence</b></li> If two Ops are form-equivalent, they are
- * defined under the same name.
- * <li><b>Structural Equivalence</b></li> If two Ops are structural-equivalent,
- * they share a common form-reduced description, searchable using
- * {@link org.scijava.ops.api.OpEnvironment#help(java.lang.String)}. For
- * example, an Op "math.add" that produces an ImgLib2 Img from Img operands, and
- * another Op "math.add" that produces a NumPy ndarray from ndarray operands,
- * will reduce to a single description "math.add" that produces an image from
- * image operands.
- * <li><b>Result Equivalence</b></li> If two Ops are result-equivalent, they
- * produce equivalent values, using the primary language-specific definition of
- * equality (e.g. `Object.equals`, for Java usage).
+ * <li><b>Form Equivalence.</b> If two Ops are form-equivalent, they are defined
+ * under the same name.</li>
+ * <li><b>Structural Equivalence.</b> If two Ops are structural-equivalent, they
+ * share a common form-reduced description, searchable using
+ * {@link OpEnvironment#help(String)}. For example, an Op "math.add" that
+ * produces an ImgLib2 Img from Img operands, and another Op "math.add" that
+ * produces a NumPy ndarray from ndarray operands, will reduce to a single
+ * description "math.add" that produces an image from image operands.</li>
+ * <li><b>Result Equivalence.</b> If two Ops are result-equivalent, they produce
+ * equivalent values, using the primary language-specific definition of equality
+ * (e.g. `Object.equals`, for Java usage).</li>
  * </ol>
+ * <p>
  * For example, consider three Ops:
+ * </p>
  * <ol>
  * <li>filter.gauss(net.imglib2.img.Img,
- * net.imglib2.type.numeric.real.FloatType) -> net.imglib2.img.Img</li>
- * <li>filter.gauss(ij.ImagePlus, java.lang.Double) -> ij.ImagePlus</li>
+ * net.imglib2.type.numeric.real.FloatType) &rarr; net.imglib2.img.Img</li>
+ * <li>filter.gauss(ij.ImagePlus, java.lang.Double) &rarr; ij.ImagePlus</li>
  * <li>filter.gauss(net.imglib2.img.Img,
- * net.imglib2.algorithm.neighborhood.Shape) -> net.imglib2.img.Img</li>
+ * net.imglib2.algorithm.neighborhood.Shape) &rarr; net.imglib2.img.Img</li>
  * </ol>
+ * <p>
  * Ops 1 and 2 are considered form-equivalent, as they have the same name, and
  * structural-equivalent, as they both take in an image data structure and a
  * floating point number and return an image data structure, but may not be
  * result-equivalent due to implementation differences between the two Ops, or
  * precision loss of the data structures.
+ * </p>
  * <p>
  * Ops 1 and 3 are also form-equivalent, as they have the same name, but are not
  * structural-equivalent, as one has an implicit Shape over which to perform a
  * gaussian blur, while the other uses an explicitly specified shape.
+ * </p>
  * <p>
  * These definitions of equivalence provide clarity when articulating the Ops
  * framework's benefits:
+ * </p>
  * <ol>
  * <li>Form-equivalence in Ops allows a comprehensive search of the available
  * algorithms for accomplishing a particular algorithm, by searching its

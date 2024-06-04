ee80dba87c30f7a3e8154b52afd4c4d0e3f3e57d
Mark Hiner <hinerm@gmail.com>
2024-06-04 13:02:48 -0500

    Update to ImgLib2 v7 + geom3D Ops to imglib2-mesh
    These two updates need to be done together, because imglib2-mesh 1.1.0,
    which adds the Ops manifest, is also updated to depend on imglib2 v7.
    
    Co-authored-by: Curtis Rueden <ctrueden@wisc.edu>

--- scijava-ops-image/src/test/java/org/scijava/ops/image/geom/geom3d/MeshFeatureTests.java
+++ scijava-ops-image/src/test/java/org/scijava/ops/image/geom/geom3d/MeshFeatureTests.java
@@ -29,24 +29,25 @@
 
 package org.scijava.ops.image.geom.geom3d;
 
-import java.util.Iterator;
-
-import net.imglib2.mesh.Mesh;
-import net.imglib2.mesh.Triangle;
-import org.scijava.ops.image.AbstractFeatureTest;
 import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.RealPoint;
+import net.imglib2.mesh.Mesh;
+import net.imglib2.mesh.MeshStats;
+import net.imglib2.mesh.Triangle;
+import net.imglib2.mesh.alg.MarchingCubesRealType;
 import net.imglib2.roi.labeling.LabelRegion;
 import net.imglib2.type.logic.BitType;
 import net.imglib2.type.numeric.real.DoubleType;
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.scijava.ops.api.OpMatchingException;
+import org.scijava.ops.image.AbstractFeatureTest;
 import org.scijava.types.Nil;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.Iterator;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MeshFeatureTests extends AbstractFeatureTest {
 
@@ -64,14 +65,26 @@ public class MeshFeatureTests extends AbstractFeatureTest {
 	public void testBoxivityMesh() {
 		// DefaultSmallestOrientedBoundingBox is not implemented.
 		assertThrows(OpMatchingException.class, () -> ops.op("geom.boxivity").input(
-			mesh).outType(DoubleType.class).apply());
+			mesh).outType(Double.class).apply());
+	}
+
+	@Test
+	public void centroid()
+	{
+		// Computed with MATLAB.
+		final double[] expected = { 5.812621359223301, 5.777346278317152, 4.818770226537216 };
+		final RealPoint centroid = ops.op("geom.centroid") //
+				.input(mesh) //
+				.outType(RealPoint.class) //
+				.apply();
+		Assertions.assertArrayEquals(expected, centroid.positionAsDoubleArray(), EPSILON);
 	}
 
 	@Test
 	public void testCompactness() {
 		// formula verified and ground truth computed with matlab
 		assertEquals(0.572416357359835, ops.op("geom.compactness").input(mesh)
-			.outType(DoubleType.class).apply().get(), EPSILON, "geom.compactness");
+			.outType(Double.class).apply(), EPSILON, "geom.compactness");
 	}
 
 	@Test
@@ -83,14 +96,13 @@ public class MeshFeatureTests extends AbstractFeatureTest {
 	public void testConvexityMesh() {
 		// formula verified and ground truth computed with matlab
 		assertEquals(0.983930494866521, ops.op("geom.convexity").input(mesh)
-			.outType(DoubleType.class).apply().get(), EPSILON, "geom.convexity");
+			.outType(Double.class).apply(), EPSILON, "geom.convexity");
 	}
 
 	@Test
 	public void testMainElongation() {
-		// formula verified and ground truth computed with matlab
-		assertEquals(0.2079585956045953, (ops.op("geom.mainElongation").input(mesh)
-			.outType(DoubleType.class).apply()).get(), EPSILON,
+		assertEquals(0.25159610503921337, (ops.op("geom.mainElongation").input(mesh)
+			.outType(Double.class).apply()), EPSILON,
 			"geom.mainElongation");
 	}
 
@@ -101,26 +113,15 @@ public class MeshFeatureTests extends AbstractFeatureTest {
 		final Iterator<Triangle> expectedFacets = mesh.triangles().iterator();
 		final Iterator<Triangle> actualFacets = result.triangles().iterator();
 		while (expectedFacets.hasNext() && actualFacets.hasNext()) {
-			final Triangle expected = expectedFacets.next();
-			final Triangle actual = actualFacets.next();
-			assertEquals(expected.v0x(), actual.v0x(), EPSILON);
-			assertEquals(expected.v0y(), actual.v0y(), EPSILON);
-			assertEquals(expected.v0z(), actual.v0z(), EPSILON);
-			assertEquals(expected.v1x(), actual.v1x(), EPSILON);
-			assertEquals(expected.v1y(), actual.v1y(), EPSILON);
-			assertEquals(expected.v1z(), actual.v1z(), EPSILON);
-			assertEquals(expected.v2x(), actual.v2x(), EPSILON);
-			assertEquals(expected.v2y(), actual.v2y(), EPSILON);
-			assertEquals(expected.v2z(), actual.v2z(), EPSILON);
+			assertTriangleEquality(expectedFacets.next(), actualFacets.next(), EPSILON);
 		}
 		assertTrue(!expectedFacets.hasNext() && !actualFacets.hasNext());
 	}
 
 	@Test
 	public void testMedianElongation() {
-		// formula verified and ground truth computed with matlab
-		assertEquals(0.30059118825775455, ops.op("geom.medianElongation").input(
-			mesh).outType(DoubleType.class).apply().get(), EPSILON,
+		assertEquals(0.1288247536, ops.op("geom.medianElongation").input(
+			mesh).outType(Double.class).apply(), EPSILON,
 			"geom.medianElongation");
 	}
 
@@ -128,49 +129,49 @@ public class MeshFeatureTests extends AbstractFeatureTest {
 	public void testSizeConvexHullMesh() {
 		// ground truth computed with matlab
 		assertEquals(304.5, ops.op("geom.sizeConvexHull").input(mesh).outType(
-			DoubleType.class).apply().get(), EPSILON, "geom.sizeConvexHull");
+			Double.class).apply(), EPSILON, "geom.sizeConvexHull");
 	}
 
 	@Test
 	public void testSizeMesh() {
 		// ground truth computed with matlab
 		assertEquals(257.5, ops.op("geom.size").input(mesh).outType(
-			DoubleType.class).apply().get(), EPSILON, "geom.size");
+			Double.class).apply(), EPSILON, "geom.size");
 	}
 
 	@Test
 	public void testSolidityMesh() {
 		// formula verified and ground truth computed with matlab
 		assertEquals(0.845648604269294, ops.op("geom.solidity").input(mesh).outType(
-			DoubleType.class).apply().get(), EPSILON, "geom.solidity");
+			Double.class).apply(), EPSILON, "geom.solidity");
 	}
 
 	@Test
 	public void testSpareness() {
 		// formula verified
-		assertEquals(0.7884710437076516, ops.op("geom.spareness").input(mesh)
-			.outType(DoubleType.class).apply().get(), EPSILON, "geom.spareness");
+		assertEquals(0.9838757743034947, ops.op("geom.spareness").input(mesh)
+			.outType(Double.class).apply(), EPSILON, "geom.spareness");
 	}
 
 	@Test
 	public void testSphericity() {
 		// formula verified and ground truth computed with matlab
 		assertEquals(0.830304411183464, ops.op("geom.sphericity").input(mesh)
-			.outType(DoubleType.class).apply().get(), EPSILON, "geom.sphericity");
+			.outType(Double.class).apply(), EPSILON, "geom.sphericity");
 	}
 
 	@Test
 	public void testSurfaceArea() {
 		// ground truth computed with matlab
 		assertEquals(235.7390893402464, ops.op("geom.boundarySize").input(mesh)
-			.outType(DoubleType.class).apply().get(), EPSILON, "geom.boundarySize");
+			.outType(Double.class).apply(), EPSILON, "geom.boundarySize");
 	}
 
 	@Test
 	public void testSurfaceAreaConvexHull() {
 		// ground truth computed with matlab
 		assertEquals(231.9508788339317, ops.op("geom.boundarySizeConvexHull").input(
-			mesh).outType(DoubleType.class).apply().get(), EPSILON,
+			mesh).outType(Double.class).apply(), EPSILON,
 			"geom.boundarySizeConvexHull");
 	}
 
@@ -178,14 +179,14 @@ public class MeshFeatureTests extends AbstractFeatureTest {
 	public void testVerticesCountConvexHullMesh() {
 		// verified with matlab
 		assertEquals(57, ops.op("geom.verticesCountConvexHull").input(mesh).outType(
-			DoubleType.class).apply().get(), EPSILON, "geom.verticesCountConvexHull");
+			Double.class).apply(), EPSILON, "geom.verticesCountConvexHull");
 	}
 
 	@Test
 	public void testVerticesCountMesh() {
 		// verified with matlab
 		assertEquals(184, ops.op("geom.verticesCount").input(mesh).outType(
-			DoubleType.class).apply().get(), EPSILON, "geom.verticesCount");
+			Double.class).apply(), EPSILON, "geom.verticesCount");
 
 	}
 
@@ -197,5 +198,57 @@ public class MeshFeatureTests extends AbstractFeatureTest {
 
 			}).apply();
 		// https://github.com/imagej/imagej-ops/issues/422
+	}
+
+	private static void assertTriangleEquality( //
+		final Triangle exp, //
+		final Triangle act, //
+		final double eps //
+	) {
+		// Determine whether triangles are inverted versions of each other
+		boolean inverted = //
+				Math.abs(exp.nx() + act.nx()) <= eps && //
+				Math.abs(exp.ny() + act.ny()) <= eps && //
+				Math.abs(exp.nz() + act.nz()) <= eps;
+
+		// Assert normal equality
+		assertEquals(exp.nx() * (inverted ? -1 : 1), act.nx(), eps);
+		assertEquals(exp.ny() * (inverted ? -1 : 1), act.ny(), eps);
+		assertEquals(exp.nz() * (inverted ? -1 : 1), act.nz(), eps);
+
+		// Assert vertices equality
+		long[] expIndices = new long[]{exp.vertex0(), exp.vertex1(), exp.vertex2()};
+		long[] actIndices = new long[]{act.vertex0(), act.vertex1(), act.vertex2()};
+
+		// find starting index
+		Mesh eM = exp.mesh();
+		Mesh aM = act.mesh();
+		int actIdx = 0;
+		for(; actIdx < 3; actIdx++) {
+			if (equal(eM, aM, expIndices[0], actIndices[actIdx], eps)) {
+				for(int i = 1; i < 3; i++) {
+					actIdx = (actIdx + (inverted ? 2 : 1)) % 3;
+					assertTrue(equal(eM, aM, expIndices[i], actIndices[actIdx], eps));
+				}
+				return;
+			}
+		}
+		Assertions.fail();
+	}
+
+	/**
+	 * Helper method comparing the equality of vertices from two different {@link Mesh}es
+	 *
+	 * @param exp one {@link Mesh}
+	 * @param act another {@link Mesh}
+	 * @param expIdx the index of some vertex in the first {@code Mesh}
+	 * @param actIdx the index of another vertex in the second {@code Mesh}
+	 * @param eps the threshold for numerical error
+	 * @return {@code true} iff the vertices are equal between {@code Mesh}es. Equality is defined as within {@code eps} in each dimension.
+	 */
+	private static boolean equal(final Mesh exp, final Mesh act, final long expIdx, final long actIdx, final double eps) {
+		return exp.vertices().nx(expIdx) - act.vertices().nx(actIdx) <= eps &&
+			exp.vertices().ny(expIdx) - act.vertices().ny(actIdx) <= eps &&
+			exp.vertices().nz(expIdx) - act.vertices().nz(actIdx) <= eps;
 	}
 }

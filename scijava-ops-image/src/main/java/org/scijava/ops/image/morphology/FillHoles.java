8772a1a05e9c08e19688e16acfd502190e62f157
Gabriel Selzer <gjselzer@wisc.edu>
2024-08-13 12:13:35 -0500

    WIP: Things work, but ugly

--- scijava-ops-image/src/main/java/org/scijava/ops/image/morphology/FillHoles.java
+++ scijava-ops-image/src/main/java/org/scijava/ops/image/morphology/FillHoles.java
@@ -34,14 +34,13 @@ import net.imglib2.IterableInterval;
 import net.imglib2.Localizable;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.algorithm.neighborhood.Shape;
-import net.imglib2.loops.LoopBuilder;
 import net.imglib2.type.BooleanType;
 import net.imglib2.util.Util;
 import net.imglib2.view.Views;
-
 import org.scijava.function.Computers;
-import org.scijava.function.Inplaces;
 import org.scijava.ops.spi.OpDependency;
+
+import java.util.ArrayList;
 
 /**
  * Fills the holes of a BooleanType image.
@@ -63,61 +62,59 @@ public class FillHoles<T extends BooleanType<T>> implements
 	private Computers.Arity1<T, RandomAccessibleInterval<T>> fillConstant;
 
 	/**
-	 * TODO
+	 * Fills holes in {@code input} by:
+	 * <ol>
+	 * <li>Setting the entire output to be foreground</li>
+	 * <li>Iteratively finding all pixels reachable from the edges, setting them to background in the output</li>
+	 * </ol>
 	 *
-	 * @param input
-	 * @param structElement
-	 * @param output
+	 * @param input the input image
+	 * @param shape a {@link Shape} defining the set of neighbors around each pixel.
+	 * @param output a pre-allocated output buffer.
 	 */
 	@Override
 	public void compute(final RandomAccessibleInterval<T> input,
-		final Shape structElement, final RandomAccessibleInterval<T> output)
+		final Shape shape, final RandomAccessibleInterval<T> output)
 	{
 		var dim = new long[output.numDimensions()];
 		output.dimensions(dim);
 
-		// Fill with non background marker
+		// Set entire output to foreground
 		T one = Util.getTypeFromInterval(output).copy();
 		one.setOne();
 		fillConstant.compute(one, output);
 
-		// Flood fill background along the edges, to exclude holes bordering an edge.
-		// What is left will be the shape with interior holes filled in.
-		var raIn = input.randomAccess();
-		var raOut = output.randomAccess();
-
-		[5, 4, 3]
-		[0, ..., ...]
-		[l_i-1, ..., ...]
-		[..., 0, ...]
-		[..., l_i-1, ...]
-		[..., ..., 0]
-		[..., ..., l_i-1]
+		// Iterate over each face
 		for (var i = 0; i < dim.length; i++) {
-			var topSlice = Views.hyperSlice(input, i, 0);
-			var bottomSlice = Views.hyperSlice(input, i, dim[i] - 1);
-			LoopBuilder.setImages(topSlice).forEachPixel(t -> {
-				floodFillComp.compute(topSlice, , structElement, output);
-			});
+			// top slice
+			fillHolesInSlice(input, i, 0, shape, output);
+			// bottom slice
+			fillHolesInSlice(input, i, dim[i] - 1, shape, output);
 		}
 
-		while (rc.hasNext()) {
-			rc.next();
-			opc.next();
-			if (!opc.get().get()) continue; // this pixel is not a background pixel
-			if (rc.get().get()) continue; // a flood fill already took care of this pixel
-			boolean border = false;
-			for (var i = 0; i < output.numDimensions(); i++) {
-				if (rc.getLongPosition(i) == 0 || rc.getLongPosition(i) == dim[i] -
-					1)
-				{
-					border = true;
-					break;
-				}
+	}
+
+	private void fillHolesInSlice(RandomAccessibleInterval<T> input, int i, long pos, Shape shape, RandomAccessibleInterval<T> output) {
+		var cursor = Views.hyperSlice(input, i, pos).cursor();
+		var raOut = output.randomAccess();
+		raOut.setPosition(pos, i);
+		int dims = input.numDimensions();
+		while (cursor.hasNext()) {
+			cursor.next();
+			if (cursor.get().get()){
+				// this pixel is not a background pixel
+				continue;
 			}
-			if (border) {
-				floodFillComp.compute(input, rc, structElement, output);
+			// FIXME This is ugly
+			for(int j = 0; j < dims - 1; j++) {
+				raOut.setPosition(cursor.getIntPosition(j), j >= i ? j + 1: j);
 			}
+			if (!raOut.get().get()){
+				// a flood fill already took care of this pixel
+				continue;
+			}
+			floodFillComp.compute(input, raOut, shape, output);
 		}
+
 	}
 }
